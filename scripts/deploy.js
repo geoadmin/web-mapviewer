4d837ef89d3adc88466d489f0bded6d51f9290df
Pascal Barth <pak.barth@gmail.com>
2020-06-17 09:42:01 +0200

    getting AWS required access key and token from env variables

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,13 @@
+#!/usr/bin/env node
+
 // Load the AWS SDK for Node.js
 const AWS = require('aws-sdk');
 // Set the region
 AWS.config.update({region: 'eu-central-1'}); // Frankfurt
 
-// forcing profile to be `swisstopo-bgdi-builder`
-const credentials = new AWS.SharedIniFileCredentials({profile: 'swisstopo-bgdi-builder'});
-if (!credentials) {
-    console.error("Profile 'swisstopo-bgdi-builder' is not defined");
-    return -1;
-}
-AWS.config.credentials = credentials;
-
 // Create S3 service object
+// Credentials are defined by env variables AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_SESSION_TOKEN
+// (don't forget to switch role if needed before running the script)
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // Call S3 to list the buckets
