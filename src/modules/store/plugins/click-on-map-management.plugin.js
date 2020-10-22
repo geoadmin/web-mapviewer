cf53ff1a0b360be3358105e7623144bacc6441c0
Pascal Barth <pak.barth@gmail.com>
2020-10-22 15:04:34 +0200

    Using a lib to detect mobile

--- src/modules/store/plugins/click-on-map-management.plugin.js
+++ src/modules/store/plugins/click-on-map-management.plugin.js
@@ -1,15 +1,17 @@
+import { isMobile } from 'mobile-device-detect';
+
 const clickOnMapManagementPlugin = store => {
     store.subscribe((mutation, state) => {
         if (mutation.type === 'setClickInfo') {
-            // if screen size is smcaller than a tablet screen, we manage long click (>500ms) as "identify"
-            // and short click (<500ms) as "fullscreen toggle"
-            if (state.size.width <= 900) {
+            // if mobile, we manage long click (>500ms) as "identify" and short click (<500ms) as "fullscreen toggle"
+            if (isMobile) {
                 if (state.map.clickInfo.millisecondsSpentMouseDown < 500) {
                     store.dispatch('toggleHeader')
                 } else {
                     // TODO: identify
                 }
             } else {
+                // for Desktop, click is always an "identify"
                 // TODO: identify
             }
         }
