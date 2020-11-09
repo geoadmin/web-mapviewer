7883209c7ad6f56c2bd4b36c1ae4f445837c7217
Pascal Barth <pak.barth@gmail.com>
2020-11-09 12:22:26 +0100

    Adding unit test covering legacy param import

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,5 +7,6 @@
 // Adds a command that visit the main view and wait for the map to be shown (for the app to be ready)
 Cypress.Commands.add('goToMapView', () => {
     cy.visit('/')
-    cy.get('[data-cy="map"]', { timeout: 5000 }).should('be.visible');
+    // we leave some room for the CI to catch the DOM element (can be a bit slow depending on the CPU power of CI's VM)
+    cy.get('[data-cy="map"]', { timeout: 10000 }).should('be.visible');
 })
