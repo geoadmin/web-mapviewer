29ec9346f1d6500344c92c02ebfe7efc67708265
Frederic Junod <frederic.junod@camptocamp.com>
2021-03-30 11:40:31 +0200

    Use performance.now instead of moment
    `performance.now` is supported everywhere:
    https://caniuse.com/high-resolution-time

--- src/modules/map/components/openlayers/OpenLayersMap.vue
+++ src/modules/map/components/openlayers/OpenLayersMap.vue
@@ -79,8 +79,6 @@
 <script>
 import 'ol/ol.css'
 
-import moment from 'moment'
-
 import { mapState, mapGetters, mapActions } from 'vuex'
 import { Map, View } from 'ol'
 import ScaleLine from 'ol/control/ScaleLine'
@@ -182,11 +180,11 @@ export default {
         let pointerDownStart = null
         let lastClickTimeLength = 0
         this.map.on('pointerdown', () => {
-            pointerDownStart = moment()
+            pointerDownStart = performance.now()
         })
         // TODO: trigger a click after pointer is down at (roughly) the same spot for longer than 1sec (no need to wait for the user to stop the click)
         this.map.on('pointerup', () => {
-            lastClickTimeLength = moment().diff(pointerDownStart)
+            lastClickTimeLength = performance.now() - pointerDownStart
             pointerDownStart = null
         })
         // using 'singleclick' event instead of 'click', otherwise a double click (for zooming) on mobile
