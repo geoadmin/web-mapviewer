c46dc33f1ed2383426b864668b62b68c59898289
Guillaume Beraudo <guillaume.beraudo@gmail.com>
2021-05-03 17:10:57 +0200

    Add basic drawing style popup

--- src/modules/drawing/DrawingModule.vue
+++ src/modules/drawing/DrawingModule.vue
@@ -1,11 +1,21 @@
 <template>
-    <div v-if="show" class="draw-overlay">
-        <DrawingToolbox
-            :drawing-modes="drawingModes"
-            :current-drawing-mode="currentDrawingMode"
-            @close="hideDrawingOverlay"
-            @setDrawingMode="changeDrawingMode"
-        />
+    <div>
+        <div v-if="show" class="draw-overlay">
+            <DrawingToolbox
+                :drawing-modes="drawingModes"
+                :current-drawing-mode="currentDrawingMode"
+                @close="hideDrawingOverlay"
+                @setDrawingMode="changeDrawingMode"
+            />
+        </div>
+        <div v-show="show">
+            <drawing-style-popup
+                ref="overlay"
+                :feature="selectedFeature"
+                @delete="deleteSelectedFeature"
+                @close="deactivateFeature"
+            />
+        </div>
     </div>
 </template>
 
@@ -15,14 +25,34 @@ import { drawingModes } from '@/modules/store/modules/drawing.store'
 import DrawingToolbox from '@/modules/drawing/components/DrawingToolbox'
 import DrawingManager from '@/modules/drawing/lib/DrawingManager'
 import { createEditingStyle } from '@/modules/drawing/lib/style'
+import DrawingStylePopup from './components/DrawingStylePopup.vue'
+import { Overlay } from 'ol'
+
+const overlay = new Overlay({})
 
 export default {
-    components: { DrawingToolbox },
+    components: { DrawingToolbox, DrawingStylePopup },
     inject: ['getMap'],
     computed: {
         ...mapState({
             show: (state) => state.ui.showDrawingOverlay,
             currentDrawingMode: (state) => state.drawing.mode,
+            geoJson: (state) => state.drawing.geoJson,
+            selectedFeature: function (state) {
+                const sfd = state.drawing.selectedFeatureData
+                const geoJson = state.drawing.geoJson
+                let f = null
+                if (sfd) {
+                    f = geoJson.features.find((f) => f.id === sfd.featureId)
+                }
+                overlay.setVisible(!!f)
+                if (!f) {
+                    return null
+                }
+                const xy = f.coordinate || [731667.39, 5862995.8]
+                overlay.setPosition(xy)
+                return f
+            },
         }),
         drawingModes: function () {
             const modes = []
@@ -31,8 +61,12 @@ export default {
         },
     },
     mounted() {
+        /** @type {import('ol/Map').default} */
+        const map = this.getMap()
+        overlay.setElement(this.$refs['overlay'].$el)
+        map.addOverlay(overlay)
         this.manager = new DrawingManager(
-            this.getMap(),
+            map,
             {
                 [drawingModes.LINE]: {
                     drawOptions: {
@@ -102,7 +136,12 @@ export default {
         })
     },
     methods: {
-        ...mapActions(['toggleDrawingOverlay', 'setDrawingMode']),
+        ...mapActions([
+            'toggleDrawingOverlay',
+            'setDrawingMode',
+            'setDrawingGeoJSON',
+            'setDrawingSelectedFeatureData',
+        ]),
         hideDrawingOverlay: function () {
             this.setDrawingMode(null)
             this.toggleDrawingOverlay()
@@ -112,6 +151,16 @@ export default {
 
             // FIXME: wrong place
             this.manager.toggleTool(mode)
+        },
+        deleteSelectedFeature: function (id) {
+            const newFeatures = this.geoJson.features.filter((f) => f.id !== id)
+            const newGeojJson = Object.assign({}, this.geoJson, {
+                features: newFeatures,
+            })
+            this.setDrawingGeoJSON(newGeojJson)
+        },
+        deactivateFeature: function () {
+            this.setDrawingSelectedFeatureData(null)
         },
     },
 }
