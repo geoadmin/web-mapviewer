f852f392575781cedcc38665c7c36abc36743609
ltkum <kunzi.martin@gmail.com>
2021-07-05 15:44:51 +0200

    transferring lang selection in a setting section, adding e2e tests
    removing commented import. Linting

---
+++ src/modules/menu/components/MenuSettings.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="menu-settings" data-cy="menu-settings-content">
+        <div id="menu-lang-selector">
+            <LangSwitchToolbar />
+        </div>
+    </div>
+</template>
+
+<script>
+import LangSwitchToolbar from '@/modules/i18n/components/LangSwitchToolbar'
+
+export default {
+    components: {
+        LangSwitchToolbar,
+    },
+}
+</script>
