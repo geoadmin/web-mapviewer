95dd097c46b10bda7dc2125ce8b3653fd3674b6e
Pascal Barth <pak.barth@gmail.com>
2021-08-30 14:38:43 +0200

    e2e test drawing fixture simplification
    Separation of drawing utilities and common utilities in two support files
    The e2e app now only serves one single icon for all requests (we don't care about testing the color of the icon, that's service-icons' responsibility)

---
+++ tests/e2e/support/drawing.js
@@ -0,0 +1,116 @@
+const olSelector = '.ol-viewport'
+
+const addIconSetsFixtureAndIntercept = () => {
+    cy.intercept(`**/v4/icons/sets`, {
+        fixture: 'service-icons/sets.fixture.json',
+    }).as('iconSets')
+}
+
+const addDefaultIconsFixtureAndIntercept = () => {
+    cy.intercept(`**/v4/icons/sets/default/icons`, {
+        fixture: 'service-icons/set-default.fixture.json',
+    }).as('iconSet-default')
+}
+
+const addSecondIconsFixtureAndIntercept = () => {
+    cy.intercept(`**/v4/icons/sets/second/icons`, {
+        fixture: 'service-icons/set-second.fixture.json',
+    }).as('iconSet-second')
+}
+
+const addIconFixtureAndIntercept = () => {
+    cy.intercept(`**/v4/icons/sets/default/icons/**@1x-255,0,0.png`, {
+        fixture: 'service-icons/placeholder.png',
+    }).as('icon-default')
+    cy.intercept(`**/v4/icons/sets/second/icons/**@1x.png`, {
+        fixture: 'service-icons/placeholder.png',
+    }).as('icon-second')
+}
+
+Cypress.Commands.add('drawGeoms', () => {
+    cy.clickDrawingTool('marker')
+    cy.get(olSelector).click(170, 190)
+
+    cy.clickDrawingTool('text')
+    cy.get(olSelector).click(200, 190)
+
+    cy.clickDrawingTool('measure')
+    cy.get(olSelector).click(100, 200).click(150, 200).click(150, 230).click(100, 200)
+
+    cy.clickDrawingTool('line')
+    cy.get(olSelector).click(210, 200).click(220, 200).dblclick(230, 230)
+})
+
+Cypress.Commands.add('goToDrawing', () => {
+    addIconFixtureAndIntercept()
+    addIconSetsFixtureAndIntercept()
+    addDefaultIconsFixtureAndIntercept()
+    addSecondIconsFixtureAndIntercept()
+    cy.goToMapView()
+    cy.get('[data-cy="menu-button"]').click()
+    cy.get('[data-cy="menu-tray-drawing-section"]').click()
+    cy.readStoreValue('state.ui.showDrawingOverlay').should('be.true')
+})
+
+Cypress.Commands.add('clickDrawingTool', (name) => {
+    expect(['marker', 'text', 'line', 'measure']).to.include(name)
+    cy.get(`[data-cy="drawing-toolbox-mode-button-${name.toUpperCase()}`).click()
+    cy.readStoreValue('state.drawing.mode').should('eq', name.toUpperCase())
+})
+
+Cypress.Commands.add('readDrawingFeatures', (type, callback) => {
+    cy.readWindowValue('drawingManager')
+        .then((manager) => manager.source.getFeatures())
+        .then((features) => {
+            expect(features).to.have.length(1)
+            const foundType = features[0].getGeometry().getType()
+            expect(foundType).to.equal(type)
+            if (callback) callback(features)
+        })
+})
+
+Cypress.Commands.add('checkDrawnGeoJsonProperty', (key, expected) => {
+    cy.readWindowValue('drawingManager')
+        .then((manager) => manager.source.getFeatures())
+        .then((features) => {
+            expect(features).to.have.length(1)
+            const v = features[0].get(key)
+            expect(v).to.equal(
+                expected,
+                `${v} != ${expected} Properties are ${JSON.stringify(
+                    features[0].getProperties(),
+                    null,
+                    2
+                )}`
+            )
+        })
+})
+Cypress.Commands.add('checkDrawnGeoJsonPropertyContains', (key, expected) => {
+    cy.readWindowValue('drawingManager')
+        .then((manager) => manager.source.getFeatures())
+        .then((features) => {
+            expect(features).to.have.length(1)
+            const v = features[0].get(key)
+            expect(v).to.contain(
+                expected,
+                `${v} != ${expected} Properties are ${JSON.stringify(
+                    features[0].getProperties(),
+                    null,
+                    2
+                )}`
+            )
+        })
+})
+
+Cypress.Commands.add('checkKMLFileResponse', (interception, data, create = false) => {
+    if (!create) {
+        const urlArray = interception.request.url.split('/')
+        const id = urlArray[urlArray.length - 1]
+        expect(id).to.be.eq('1234_adminId')
+    }
+    expect(interception.request.headers['content-type']).to.be.eq(
+        'application/vnd.google-earth.kml+xml'
+    )
+    expect(interception.request.body).to.contain('</kml>')
+    data.forEach((text) => expect(interception.request.body).to.contain(text))
+})
