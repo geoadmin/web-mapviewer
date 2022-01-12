601900c7ea122d43015ea1c7017110d6d01ea278
Pascal Barth <pak.barth@gmail.com>
2022-01-12 08:56:45 +0100

    BGDIINF_SB-2114: URL decoding legacy layers param so that if pipes (|) is used (for instance with an external KML layer) and that it was encoded by vue router, or another library in between, it is URL decoded before transformation into the new layers URL param syntax

--- src/utils/legacyLayerParamUtils.js
+++ src/utils/legacyLayerParamUtils.js
@@ -66,38 +66,45 @@ export function getLayersFromLegacyUrlParams(layersConfig, legacyLayersParam) {
         }
 
         if (layerIdsUrlParam) {
-            layerIdsUrlParam.split(',').forEach((layerId, index) => {
-                let layer = layersConfig.find((layer) => layer.getID() === layerId)
-                // if this layer can be found in the list of GeoAdminLayers (from the config), we use that as the basis
-                // to add it to the map
-                if (layer) {
-                    // we can't modify "layer" straight because it comes from the Vuex state, so we deep copy it
-                    // in order to alter it before returning it
-                    layer = layer.clone()
-                }
-                if (layerId.startsWith('KML||')) {
-                    const kmlLayerParts = decodeURIComponent(layerId).split('||')
-                    layer = new KMLLayer(i18n.global.t('draw_layer_label'), 1.0, kmlLayerParts[1])
-                }
-                if (layer) {
-                    // checking if visibility is set in URL
-                    if (layerVisibilities.length > index) {
-                        layer.visible = layerVisibilities[index] === 'true'
-                    } else {
-                        // if param layers_visibility is not present, it means all layers are visible
-                        layer.visible = true
+            layerIdsUrlParam
+                .split(',')
+                .map(decodeURI)
+                .forEach((layerId, index) => {
+                    let layer = layersConfig.find((layer) => layer.getID() === layerId)
+                    // if this layer can be found in the list of GeoAdminLayers (from the config), we use that as the basis
+                    // to add it to the map
+                    if (layer) {
+                        // we can't modify "layer" straight because it comes from the Vuex state, so we deep copy it
+                        // in order to alter it before returning it
+                        layer = layer.clone()
                     }
-                    // checking if opacity is set in the URL
-                    if (layerOpacities.length > index) {
-                        layer.opacity = Number(layerOpacities[index])
+                    if (layerId.startsWith('KML||')) {
+                        const kmlLayerParts = decodeURIComponent(layerId).split('||')
+                        layer = new KMLLayer(
+                            i18n.global.t('draw_layer_label'),
+                            1.0,
+                            kmlLayerParts[1]
+                        )
                     }
-                    // checking if a timestamp is defined for this layer
-                    if (layerTimestamps.length > index && layerTimestamps[index]) {
-                        layer.timeConfig.currentTimestamp = layerTimestamps[index]
+                    if (layer) {
+                        // checking if visibility is set in URL
+                        if (layerVisibilities.length > index) {
+                            layer.visible = layerVisibilities[index] === 'true'
+                        } else {
+                            // if param layers_visibility is not present, it means all layers are visible
+                            layer.visible = true
+                        }
+                        // checking if opacity is set in the URL
+                        if (layerOpacities.length > index) {
+                            layer.opacity = Number(layerOpacities[index])
+                        }
+                        // checking if a timestamp is defined for this layer
+                        if (layerTimestamps.length > index && layerTimestamps[index]) {
+                            layer.timeConfig.currentTimestamp = layerTimestamps[index]
+                        }
+                        layersToBeActivated.push(layer)
                     }
-                    layersToBeActivated.push(layer)
-                }
-            })
+                })
         }
     }
     return layersToBeActivated
