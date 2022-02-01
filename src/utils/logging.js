9b6516a976561c2c91a5543b235ec62cc0bd7c4f
Florian Mäder <florianklemens.maeder@swisstopo.ch>
2022-02-01 14:44:05 +0100

    BGDIINF_SB-2161: Update logging to log errors in production
    This updates the log function to ignore DEBUG for LogLevel.ERROR.
    
    It also updates the JSDoc comments and changes the enum values to lower-case to make the thing backwards compatible.

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,48 +1,54 @@
 import { DEBUG } from '../config'
 
 /**
- * A logging level reference can be passed as first parameter to the log function. The levels
- * correspond to the nativr console methods.
+ * A log level reference that can be passed as first parameter to the log function. The levels
+ * correspond to the native console methods and their log level.
+ *
+ * @enum {String}
+ * @readonly
  */
-export const LOGLEVEL = {
-    ERROR: 'ERROR',
-    INFO: 'INFO',
-    DEBUG: 'DEBUG',
+export const LogLevel = {
+    ERROR: 'error',
+    INFO: 'info',
+    DEBUG: 'debug',
 }
 
 /**
- * Logs something if the env is not PROD, otherwise does nothing.
+ * Logs the provided parameters to the console. To set a different log level pass the desired
+ * LogLevel as first parameter.
  *
- * @param {String} [level]
- * @param {Boolean | String | Number | Object} message
+ * In production builds only calls with LogLevel.ERROR will be logged!
+ *
+ * @param {LogLevel} level
+ * @param {...any} message
  */
-const log = function (level, ...message) {
+export default function log(level, ...message) {
+    // In production we only log errors.
+    if (!DEBUG && level !== LogLevel.ERROR) {
+        return
+    }
+
     /* eslint-disable no-console */
-    if (DEBUG) {
-        switch (level) {
-            case LOGLEVEL.ERROR:
-                console.error(...message)
-                break
-            case LOGLEVEL.INFO:
-                console.info(...message)
-                break
-            case LOGLEVEL.DEBUG:
-                console.debug(...message)
-                break
-            default:
-                console.log(level, ...message)
-        }
+    switch (level) {
+        case LogLevel.ERROR:
+            console.error(...message)
+            break
+        case LogLevel.INFO:
+            console.info(...message)
+            break
+        case LogLevel.DEBUG:
+            console.debug(...message)
+            break
+        default:
+            // We assume that the first parameter wasn't the level but part of the message.
+            console.log(level, ...message)
     }
     /* eslint-enable  no-console */
 }
 
-/* eslint-disable no-console */
-/** Same as console.error but doesn't log in production. */
-log.error = (...message) => console.error(...message)
-/** Same as console.info but doesn't log in production. */
-log.info = (...message) => console.info(...message)
-/** Same as console.debug but doesn't log in production. */
-log.debug = (...message) => console.debug(...message)
-/* eslint-enable  no-console */
-
-export default log
+/** Shorthand for log('error', ...message) */
+log.error = (...message) => log(LogLevel.ERROR, ...message)
+/** Shorthand for log('info', ...message) */
+log.info = (...message) => log(LogLevel.INFO, ...message)
+/** Shorthand for log('debug', ...message) */
+log.debug = (...message) => log(LogLevel.DEBUG, ...message)
