fb28e608485f7cecef7c6874e49d9014aac7677d
Pascal Barth <pak.barth@gmail.com>
2022-04-01 14:39:38 +0200

    Separating DrawingManager logic into many components
    Creating a component per OpenLayers interaction
    Using a mixin to gather all common interaction code in one place

--- src/modules/drawing/components/DrawingTooltip.vue
+++ src/modules/drawing/components/DrawingTooltip.vue
@@ -7,15 +7,15 @@ import { Polygon } from 'ol/geom'
 import Overlay from 'ol/Overlay'
 
 export default {
-    inject: ['getMap', 'getDrawingLayer', 'getSelectInteraction', 'getModifyInteraction'],
+    inject: ['getMap'],
     props: {
         currentDrawingMode: {
             type: String,
             default: null,
         },
-        currentlyEditedFeature: {
-            type: Object,
-            default: null,
+        selectedFeatures: {
+            type: Array,
+            default: () => [],
         },
     },
     data() {
@@ -61,16 +61,13 @@ export default {
             this.tooltipOverlay.setPosition(event.coordinate)
             // detecting features under the mouse cursor
             const map = this.getMap()
-            const modifyInteraction = this.getModifyInteraction()
-            const selectInteraction = this.getSelectInteraction()
-            if (map && modifyInteraction && selectInteraction) {
-                const hoveringVertex = modifyInteraction.snappedToVertex_
+            if (map) {
                 let hoveringSelectableFeature = false
                 let hoveringSelectedFeature = false
 
-                const hasFeatureSelected = selectInteraction.getFeatures().getLength() > 0
+                const hasFeatureSelected = this.selectedFeatures && this.selectedFeatures.length > 0
                 // we only keep track of the first feature's info (the one on top of the stack)
-                const selectedFeatureId = selectInteraction.getFeatures().item(0)?.getId()
+                const selectedFeatureId = hasFeatureSelected && this.selectedFeatures[0].id
                 let featureUnderCursor
 
                 map.forEachFeatureAtPixel(event.pixel, (feature, layer) => {
@@ -93,17 +90,14 @@ export default {
                 // detecting if we are hovering the area of a polygon (and not its borders)
                 // if so, we suppress the flag "hoveringSelectedFeature" as it is of no value
                 // to know that we are hovering the area (only borders have interactivity)
-                if (
-                    featureUnderCursor?.getGeometry() instanceof Polygon &&
-                    modifyInteraction.vertexFeature_ === null
-                ) {
+                if (featureUnderCursor?.getGeometry() instanceof Polygon) {
                     hoveringSelectedFeature = false
                 }
 
                 const featureType = featureUnderCursor?.get('drawingMode')?.toLowerCase()
                 if (hoveringSelectedFeature) {
                     // Display a help tooltip when modifying
-                    if (hoveringVertex || ['marker', 'annotation'].includes(featureType)) {
+                    if (['marker', 'annotation'].includes(featureType)) {
                         this.updateTooltipText(`modify_existing_vertex_${featureType}`)
                     } else {
                         this.updateTooltipText(`modify_new_vertex_${featureType}`)
@@ -118,41 +112,7 @@ export default {
                 } else {
                     // Display a help tooltip when drawing
                     // TODO: help for each drawing mode
-                    if (this.currentlyEditedFeature) {
-                        const geom = this.currentlyEditedFeature.getGeometry()
-                        if (geom instanceof Polygon) {
-                            // The sketched polygon is always closed, so we remove the last coordinate.
-                            const lineCoords = geom.getCoordinates()[0].slice(0, -1)
-                            if (this.sketchPoints !== lineCoords.length) {
-                                // A point is added or removed
-                                this.sketchPoints = lineCoords.length
-                            } else if (lineCoords.length > 1) {
-                                const firstPoint = lineCoords[0]
-                                const lastPoint = lineCoords[lineCoords.length - 1]
-                                const sketchPoint = lineCoords[lineCoords.length - 2]
-
-                                // Checks is snapped to first point of geom
-                                const isSnapOnFirstPoint =
-                                    lastPoint[0] === firstPoint[0] && lastPoint[1] === firstPoint[1]
-
-                                // Checks is snapped to last point of geom
-                                const isSnapOnLastPoint =
-                                    lastPoint[0] === sketchPoint[0] &&
-                                    lastPoint[1] === sketchPoint[1]
-
-                                this.isFinishOnFirstPoint_ =
-                                    !isSnapOnLastPoint && isSnapOnFirstPoint
-
-                                // this.updateHelpTooltip(
-                                //     type,
-                                //     true,
-                                //     this.tools[type].minPoints_ < lineCoords.length,
-                                //     this.isFinishOnFirstPoint_,
-                                //     isSnapOnLastPoint
-                                // )
-                            }
-                        }
-                    } else if (this.currentDrawingMode) {
+                    if (this.currentDrawingMode) {
                         this.updateTooltipText(
                             `draw_start_${this.currentDrawingMode.toLowerCase()}`
                         )
