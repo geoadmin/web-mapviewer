384db363ac02544d09947bf15a4c5d74ee1beefc
Brice Schaffner <brice.schaffner@swisstopo.ch>
2022-11-10 20:22:43 +0100

    BGDIINF_SB-2671: Put the assets in a versioned directory
    This allow a better deployment approach on S3. We can copy first the versioned
    assets directory (containing the images but also the javascript and css code)
    into S3 without disturbing the current version, once it is copied, we
    can copy the public files from the root and finally the index.html.
    
    This procedure reduce the risk of downtime.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import generateBuildInfo from './vite-plugins/generate-build-info'
 
-const appVersion = 'v' + gitDescribeSync().semverString
+// We take the version from APP_VERSION but if not set, then take
+// it from git describe command
+let appVersion = process.env.APP_VERSION
+if (!appVersion) {
+    appVersion = 'v' + gitDescribeSync().semverString
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     return {
         base: './',
+        build: {
+            emptyOutDir: true,
+            assetsDir: `${appVersion}/assets`,
+        },
         plugins: [vue(), generateBuildInfo(appVersion)],
         resolve: {
             alias: {
@@ -26,3 +35,4 @@ export default defineConfig(({ command, mode }) => {
         },
     }
 })
+
