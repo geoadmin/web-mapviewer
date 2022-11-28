8b1af5ffb482a3129f916f30b39b7a5f0d9a916b
Brice Schaffner <brice.schaffner@swisstopo.ch>
2022-11-28 06:46:52 +0100

    BGDIINF_SB-2662: Added border to the position popup triangle
    This border is needed to increase contrast on hell background otherwise we
    don't see the position triangle on light background.

--- src/modules/map/components/LocationPopup.vue
+++ src/modules/map/components/LocationPopup.vue
@@ -270,6 +270,7 @@ export default {
 <style lang="scss" scoped>
 @import 'src/scss/webmapviewer-bootstrap-theme';
 .location-popup {
+    // Triangle border
     &::before {
         $arrow-height: 15px;
         position: absolute;
@@ -277,9 +278,20 @@ export default {
         left: 50%;
         margin-left: -$arrow-height;
         border: $arrow-height solid transparent;
-        border-bottom-color: $light;
+        border-bottom-color: var(--bs-border-color-translucent);
         pointer-events: none;
         content: '';
+    }
+    // Triangle background
+    &::after {
+        $arrow-border-height: 14px;
+        content: '';
+        border: $arrow-border-height solid transparent;
+        border-bottom-color: $light;
+        position: absolute;
+        top: -($arrow-border-height * 2);
+        left: 50%;
+        margin-left: -$arrow-border-height;
     }
     &-coordinates {
         display: grid;
