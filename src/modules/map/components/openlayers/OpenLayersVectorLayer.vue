aa0a5f40f33c02ec6b3bb762a9c00c58d450624c
Pascal Barth <pak.barth@gmail.com>
2022-12-06 12:07:07 +0100

    BGDIINF_SB-2305 : add Sentinel2 WMTS layer
    In order not to have our SwissImage WMTS cover the whole wolrd in white tiles (that's what MapLibre draws when a JPEG layer has nothing to give), I've set the bounds of our WMTS layer within MapLibre Style to some arbitrary bounds derived from LV95 bounds. I had make bounds shorter, otherwise some white background was covering Sentinel2 WMTS tiles at some places (you can still see the curvature of Mercator vs. LV95 white portion on top, I wasn't able to remove it...)

--- src/modules/map/components/openlayers/OpenLayersVectorLayer.vue
+++ src/modules/map/components/openlayers/OpenLayersVectorLayer.vue
@@ -5,8 +5,11 @@
 </template>
 
 <script>
+import { TILEGRID_EXTENT, VECTOR_TILES_IMAGERY_STYLE_ID } from '@/config'
+import { CoordinateSystems } from '@/utils/coordinateUtils'
 import MapLibreLayer from '@geoblocks/ol-maplibre-layer'
 import axios from 'axios'
+import proj4 from 'proj4'
 import addLayerToMapMixin from './utils/addLayerToMap-mixins'
 
 /** Renders a Vector layer on the map with MapLibre */
@@ -37,7 +40,7 @@ export default {
     computed: {
         mapLibreInstance() {
             return this.layer?.maplibreMap
-        }
+        },
     },
     watch: {
         opacity(newOpacity) {
@@ -63,14 +66,47 @@ export default {
                 axios.get(styleUrl).then((response) => {
                     const vectorStyle = response.data
                     vectorStyle.layers = vectorStyle.layers.filter(
-                      (layer) => layer.source !== this.excludeSource
+                        (layer) => layer.source !== this.excludeSource
                     )
+                    this.mapLibreInstance.setStyle(vectorStyle)
+                })
+            } else if (this.layerId === VECTOR_TILES_IMAGERY_STYLE_ID) {
+                // special case here, as the imagery is only over Switzerland (for now)
+                // we inject a fair-use WMTS that covers the globe under our aerial images
+                axios.get(styleUrl).then((response) => {
+                    const vectorStyle = response.data
+                    // settings bounds for swissimage, otherwise it covers the whole world with white tiles
+                    vectorStyle.sources.swissimage_wmts.bounds = [
+                        ...proj4(CoordinateSystems.LV95.epsg, CoordinateSystems.WGS84.epsg, [
+                            TILEGRID_EXTENT[0] + 50000,
+                            TILEGRID_EXTENT[1],
+                        ]),
+                        ...proj4(CoordinateSystems.LV95.epsg, CoordinateSystems.WGS84.epsg, [
+                            TILEGRID_EXTENT[2] - 20000,
+                            TILEGRID_EXTENT[3],
+                        ]),
+                    ]
+                    // setting up Sentinel2 WMTS to cover the globe outside of Switzerland
+                    vectorStyle.sources['sentinel2_wmts'] = {
+                        minzoom: 0,
+                        maxzoom: 22,
+                        tileSize: 256,
+                        type: 'raster',
+                        tiles: [
+                            'https://tiles.maps.eox.at/wmts?layer=s2cloudless-2020_3857&style=default&tilematrixset=g&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}',
+                        ],
+                    }
+                    vectorStyle.layers.splice(1, 0, {
+                        id: 'sentinel2',
+                        source: 'sentinel2_wmts',
+                        type: 'raster',
+                    })
                     this.mapLibreInstance.setStyle(vectorStyle)
                 })
             } else {
                 this.mapLibreInstance.setStyle(styleUrl)
             }
-        }
-    }
+        },
+    },
 }
 </script>
