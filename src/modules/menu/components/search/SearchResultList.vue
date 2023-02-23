7ead9e6f6bf6fdfe0f8809c44846ad1c9bcd46ee
Jérémie De Faveri <jeremie.defaveri@swisstopo.ch>
2023-02-23 12:58:15 +0100

    BGDIINF_SB-2789: Search results take more screen real-estate
    - Like with the menu, the categories of the search results now also
      split evenly depending solely on the max height defined for the whole
      search results. Scroll bars for subcategories only appear if they are
      needed.
    - Search results take normally 50% of the screen only, so that the user
      can preview the layers by hovering over them. However, if the screen
      is very small (like the landscape mode of phones), the search results take
      more than 50% (but max 100%) of the screen to still be usable.

--- src/modules/menu/components/search/SearchResultList.vue
+++ src/modules/menu/components/search/SearchResultList.vue
@@ -1,30 +1,37 @@
 <template>
     <div
-        id="search-results"
-        :class="[
-            isPhoneMode ? ['border-top', 'border-bottom', 'rounded-bottom'] : ['border', 'rounded'],
-            { 'search-results-dev-site-warning': hasDevSiteWarning && isPhoneMode },
-            ['bg-light'],
-        ]"
-        data-cy="search-results"
-        @keydown.esc.prevent="$emit('close')"
+        id="search-results-container"
+        :class="{ 'search-results-dev-site-warning': hasDevSiteWarning && isPhoneMode }"
     >
-        <SearchResultCategory
-            :title="$t('locations_results_header')"
-            :half-size="results.locationResults.length > 0"
-            :entries="results.locationResults"
-            data-cy="search-results-locations"
-            @preview-start="setPinnedLocation"
-            @preview-stop="clearPinnedLocation"
-        />
-        <SearchResultCategory
-            :title="$t('layers_results_header')"
-            :half-size="results.layerResults.length > 0"
-            :entries="results.layerResults"
-            data-cy="search-results-layers"
-            @preview-start="setPreviewLayer"
-            @preview-stop="clearPreviewLayer"
-        />
+        <div
+            class="search-results bg-light"
+            :class="
+                isPhoneMode
+                    ? ['border-top', 'border-bottom', 'rounded-bottom']
+                    : ['border', 'rounded']
+            "
+            data-cy="search-results"
+            @keydown.esc.prevent="$emit('close')"
+        >
+            <div class="search-results-inner">
+                <SearchResultCategory
+                    :title="$t('locations_results_header')"
+                    :half-size="results.locationResults.length > 0"
+                    :entries="results.locationResults"
+                    data-cy="search-results-locations"
+                    @preview-start="setPinnedLocation"
+                    @preview-stop="clearPinnedLocation"
+                />
+                <SearchResultCategory
+                    :title="$t('layers_results_header')"
+                    :half-size="results.layerResults.length > 0"
+                    :entries="results.layerResults"
+                    data-cy="search-results-layers"
+                    @preview-start="setPreviewLayer"
+                    @preview-stop="clearPreviewLayer"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
@@ -61,36 +68,47 @@ export default {
 @import 'src/scss/media-query.mixin';
 @import 'src/scss/variables';
 
-#search-results {
+#search-results-container {
     position: fixed;
     top: $header-height;
-    overflow: hidden;
-    left: 0%;
+    // 45vh (so that previews are visible), but on small screen min 20rem (ca. 3 lines per category)
+    height: min(calc(100vh - $header-height), max(20rem, 45vh));
+    left: 0;
     width: 100%;
-    max-height: calc(75vh - 3rem);
     margin: 0; //overwrites a bootstrap property from parent module
-    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    pointer-events: none;
 }
-
 .search-results-dev-site-warning {
-    // Must be important as it needs to overrite the top defined in #search-results
+    // Must be important as it needs to overrite the top defined in #search-results-container
     top: $header-height + $dev-disclaimer-height !important;
+    height: min(calc(100vh - $header-height - $dev-disclaimer-height), max(20rem, 45vh)) !important;
     // Put the search results under the rest of the header so hovering over the warning works
     // correctly
     z-index: -1;
 }
+
+.search-results {
+    display: grid;
+    grid-auto-rows: 1fr;
+    max-height: 100%;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    pointer-events: all;
+}
+
+.search-results-inner {
+    display: grid;
+    //Split evenly between all search result categories
+    grid-auto-rows: auto;
+    overflow: hidden;
+}
+
 @include respond-above(phone) {
-    #search-results {
+    #search-results-container {
         position: absolute;
         top: 100%;
     }
     .search-results-dev-site-warning {
         z-index: initial;
-    }
-}
-@include respond-above(lg) {
-    #search-results {
-        max-height: calc(75vh - 6rem);
     }
 }
 </style>
