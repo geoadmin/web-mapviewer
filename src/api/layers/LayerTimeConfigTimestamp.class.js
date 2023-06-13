1bf3cf5be35e28719ddb6d004a1713d0f5b82ec1
Pascal Barth <pak.barth@gmail.com>
2023-06-13 10:58:59 +0200

    BGDIINF_SB-2808 : timestamp/year parsing simplification
    storing the timestamp "as is" simplifies this logic significantly (no need for this getter and enum anymore)

--- src/api/layers/LayerTimeConfigTimestamp.class.js
+++ src/api/layers/LayerTimeConfigTimestamp.class.js
@@ -1,33 +1,7 @@
-/**
- * Old timestamp were covering a full year but were described as a single day of this year.
- *
- * Some timestamps were using the first of january, and others the last day of december. This enum
- * helps decipher which one to use on a time config base.
- *
- * @enum
- */
-export const TIMESTAMP_LEGACY_BEHAVIOR = {
-    START_OF_YEAR: 'START_OF_YEAR',
-    END_OF_YEAR: 'END_OF_YEAR',
-}
-
 export class LayerTimeConfigTimestamp {
-    /**
-     * @param {Number} year The year of this timestamp as YYYYY
-     * @param {TIMESTAMP_LEGACY_BEHAVIOR} legacyBehavior How the year should be filled to create the
-     *   matching timestamp in our infrastructure. Some layers have the first day of the year as
-     *   timestamp while others have the last.
-     */
-    constructor(year, legacyBehavior = TIMESTAMP_LEGACY_BEHAVIOR.END_OF_YEAR) {
-        this.year = year
-        this.legacyBehaviour = legacyBehavior
-    }
-
-    get timestamp() {
-        if (this.legacyBehaviour === TIMESTAMP_LEGACY_BEHAVIOR.START_OF_YEAR) {
-            return `${this.year}0101`
-        } else {
-            return `${this.year}1231`
-        }
+    /** @param {Number} timestamp A full timestamp as YYYYYMMDD */
+    constructor(timestamp) {
+        this.timestamp = timestamp
+        this.year = parseInt(timestamp.substring(0, 4)) || 9999
     }
 }
