2441b7bb57b6288dc9c37337a30c1b05a47ab6f7
Pascal Barth <pak.barth@gmail.com>
2023-07-12 09:38:09 +0200

    BGDIINF_SB-2949: adding 3D bucket base URL for each staging

--- src/modules/map/components/cesium/constants.js
+++ src/modules/map/components/cesium/constants.js
@@ -1,6 +1,8 @@
+import { BASE_URL_3D_TILES } from '@/config'
+
 /**
  * 3D terrain URL
  *
  * @type {string}
  */
-export const TERRAIN_URL = 'https://sys-3d.dev.bgdi.ch/ch.swisstopo.terrain.3d/v1/'
+export const TERRAIN_URL = `${BASE_URL_3D_TILES}/ch.swisstopo.terrain.3d/v1/`
