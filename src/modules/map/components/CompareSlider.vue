9be35d0422c10b4a1b4a10fe11eeac849cc7b9cc
ltkum <kunzi.martin@gmail.com>
2023-10-19 08:15:31 +0200

    BGDIINF_SB-3106 : Adding a compare slider
    Done :
    	Creating the line and allowing it to be visible
    	Added placeholder icons for the left and right
    	Added an Anchor to the openlayers map, as we switched its
    position to absolute to allow the slider to be visible
    
    To be Done  :
    	Use correct icons
    	Create a function that allows it to slide
    
    correct icons, now I need to make it so the line is perfectly in the middle of its container, with the two icons on the sides
    
    icons well positionned
    
    using a correct class name for compare slider
    
    running prettier on one file to make the linter happy
    
    setting carets to absolute, otherwise the right caret doesn't show up
    
    correction to css
    
    linting
    
    BGDIINF_SB-3206 : Adding a compare slider
    
    Done :
    	Creating the line and allowing it to be visible
    	Added placeholder icons for the left and right
    	Added an Anchor to the openlayers map, as we switched its
    position to absolute to allow the slider to be visible
    
    To be Done  :
    	Use correct icons
    	Create a function that allows it to slide

---
+++ src/modules/map/components/CompareSlider.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="test-slider">
+        <!--<FontAwesomeIcon :icon="['fas', 'square-caret-left']" class="compare-slider-caret-left" /> -->
+        <div class="compare-slider-line"></div>
+        <!--<FontAwesomeIcon :icon="['fas', 'square-caret-right']" class="compare-slider-caret-right" /> -->
+        <FontAwesomeIcon class="compare-slider-caret-left" :icon="['fas', 'arrow-left']" />
+        <FontAwesomeIcon
+            class="compare-slider-caret-right"
+            :icon="['fas', 'arrow-left']"
+            :rotation="180"
+        />
+    </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+export default {
+    components: {
+        FontAwesomeIcon,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'src/scss/webmapviewer-bootstrap-theme';
+
+$slider-position: 50%;
+
+.compare-slider {
+    position: absolute;
+    top: 0;
+    display: inline-block;
+    width: 40px;
+    height: 100%;
+    z-index: $zindex-compare-slider;
+
+    &-caret-left,
+    &-caret-right {
+        position: inherit;
+        top: 50%;
+        width: 14px;
+        height: 14px;
+        color: $white;
+        background: $primary;
+        z-index: inherit;
+    }
+
+    &-caret-left {
+        translate: 4px;
+    }
+    &-caret-right {
+        translate: 22px;
+    }
+    &-line {
+        position: relative;
+        margin: auto;
+        width: 4px;
+        height: 100%;
+        background: $primary;
+        z-index: inherit;
+    }
+}
+</style>
