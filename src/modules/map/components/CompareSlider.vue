6635e7584dec7dc58adfb5ae17c6b03c92e3d174
ltkum <kunzi.martin@gmail.com>
2023-10-19 08:34:40 +0200

    correct icons, now I need to make it so the line is perfectly in the middle of its container, with the two icons on the sides

--- src/modules/map/components/CompareSlider.vue
+++ src/modules/map/components/CompareSlider.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="test-slider">
-        <!--<FontAwesomeIcon :icon="['fas', 'square-caret-left']" class="compare-slider-caret-left" /> -->
+        <font-awesome-icon
+            class="compare-slider-caret-left"
+            :icon="['fas', 'caret-up']"
+            :rotation="270"
+        />
         <div class="compare-slider-line"></div>
-        <!--<FontAwesomeIcon :icon="['fas', 'square-caret-right']" class="compare-slider-caret-right" /> -->
-        <FontAwesomeIcon class="compare-slider-caret-left" :icon="['fas', 'arrow-left']" />
-        <FontAwesomeIcon
+
+        <font-awesome-icon
             class="compare-slider-caret-right"
-            :icon="['fas', 'arrow-left']"
-            :rotation="180"
+            :icon="['fas', 'caret-up']"
+            :rotation="90"
         />
     </div>
 </template>
@@ -25,36 +28,45 @@ export default {
 <style lang="scss" scoped>
 @import 'src/scss/webmapviewer-bootstrap-theme';
 
+$slider-position: 50%;
+
 .test-slider {
-    position: relative;
+    position: absolute;
+    top: 0;
     display: inline-block;
-    width: 32px;
+    width: 40px;
     height: 100%;
-    left: 50%;
+    left: $slider-position;
     border-top: 10px solid transparent;
     border-bottom: 10px solid transparent;
     z-index: $zindex-compare-slider;
 }
 .compare-slider-caret-left {
     position: relative;
+    margin: 2px;
     top: 50%;
-    margin-right: 4px;
     z-index: $zindex-compare-slider;
-    color: red;
+    width: 14px;
+    height: 14px;
+    color: white;
+    background: red;
 }
 
 .compare-slider-caret-right {
     position: relative;
     top: 50%;
-    margin-left: -2 px;
+    margin: 2px;
     z-index: $zindex-compare-slider;
-    color: red;
+    width: 14px;
+    height: 14px;
+    color: white;
+    background: red;
 }
 .compare-slider-line {
     position: absolute;
     width: 4px;
     height: 100%;
-    left: 50%;
+    left: $slider-position;
     margin: 2px;
     z-index: $zindex-compare-slider;
     background-color: red;
