e2a5f268e30aba99df27ebc444fd17d8562261ab
Pascal Barth <pak.barth@gmail.com>
2023-10-30 13:51:37 +0100

    BGDIINF_SB-2890 : do not use magic numbers in coordinate test anymore
    but base it on the default projection's center, and some re-projection pre-work

--- tests/e2e-cypress/integration/search/coordinates-search.cy.js
+++ tests/e2e-cypress/integration/search/coordinates-search.cy.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
 
+import { DEFAULT_PROJECTION } from '@/config'
+import { LV03, LV95, WEBMERCATOR, WGS84 } from '@/utils/coordinates/coordinateSystems'
+import CustomCoordinateSystem from '@/utils/coordinates/CustomCoordinateSystem.class'
+import { STANDARD_ZOOM_LEVEL_1_25000_MAP } from '@/utils/coordinates/SwissCoordinateSystem.class'
+import proj4 from 'proj4'
+
 const searchbarSelector = '[data-cy="searchbar"]'
 
 describe('Testing coordinates typing in search bar', { testIsolation: false }, () => {
-    // in order to ease test run, we only load the page once at the begining of this context
+    // in order to ease test run, we only load the page once at the beginning of this context
     // so that it doesn't load the page for each copy/paste in the search bar
     before(() => {
         cy.goToMapView()
@@ -15,35 +21,49 @@ describe('Testing coordinates typing in search bar', { testIsolation: false }, (
                 cy.get('[data-cy="searchbar-clear"]').click()
                 // replacing the view somewhere else in order to check that
                 // subsequent coordinate search will place the view at the correct location
-                cy.writeStoreValue('setZoom', 10)
-                cy.writeStoreValue('setCenter', [46.5, 8])
+                cy.writeStoreValue('setZoom', DEFAULT_PROJECTION.getDefaultZoom())
+                cy.writeStoreValue('setCenter', DEFAULT_PROJECTION.bounds.center)
             }
         })
     })
-    // using the same values as coordinateUtils.spec.js (in Unit cypress)
-    const coordEpsg3857 = [773900, 5976445]
-    const WGS84 = [47.2101583, 6.952062]
-    const LV95 = [2563138.69, 1228917.22]
-    const LV03 = [563138.65, 228917.28]
+    const expectedCenter = DEFAULT_PROJECTION.bounds.center.map((value) => value - 1000)
+    const expectedCenterLV95 = proj4(DEFAULT_PROJECTION.epsg, LV03.epsg, expectedCenter).map(
+        LV03.roundCoordinateValue
+    )
+    const expectedCenterLV03 = proj4(DEFAULT_PROJECTION.epsg, LV03.epsg, expectedCenter).map(
+        LV03.roundCoordinateValue
+    )
+    const expectedCenterWebMercator = proj4(
+        DEFAULT_PROJECTION.epsg,
+        WEBMERCATOR.epsg,
+        expectedCenter
+    ).map(WEBMERCATOR.roundCoordinateValue)
+    const centerWGS84 = proj4(DEFAULT_PROJECTION.epsg, WGS84.epsg, expectedCenter).map(
+        WGS84.roundCoordinateValue
+    )
 
     const checkCenterInStore = (acceptableDelta = 0.0) => {
         cy.readStoreValue('state.position.center').should((center) => {
-            expect(center[0]).to.be.approximately(coordEpsg3857[0], acceptableDelta)
-            expect(center[1]).to.be.approximately(coordEpsg3857[1], acceptableDelta)
+            expect(center[0]).to.be.approximately(expectedCenter[0], acceptableDelta)
+            expect(center[1]).to.be.approximately(expectedCenter[1], acceptableDelta)
         })
     }
     const checkZoomLevelInStore = () => {
         // checking that the zoom level is at the 1:25'000 map level after a coordinate input in the search bar
-        // in world-wide zoom level, it means a 15.5 zoom level (in LV95 zoom level it is 8)
-        cy.readStoreValue('state.position.zoom').should('be.eq', 15.5)
+        let expectedZoomLevel = STANDARD_ZOOM_LEVEL_1_25000_MAP
+        if (DEFAULT_PROJECTION instanceof CustomCoordinateSystem) {
+            expectedZoomLevel =
+                DEFAULT_PROJECTION.transformStandardZoomLevelToCustom(expectedZoomLevel)
+        }
+        cy.readStoreValue('state.position.zoom').should('be.eq', expectedZoomLevel)
     }
     const checkThatCoordinateAreHighlighted = (acceptableDelta = 0.0) => {
         // checking that a balloon marker has been put on the coordinate location (that it is a highlighted location in the store)
         cy.readStoreValue('state.map.pinnedLocation').should((feature) => {
             expect(feature).to.not.be.null
             expect(feature).to.be.a('array').that.is.not.empty
-            expect(feature[0]).to.be.approximately(coordEpsg3857[0], acceptableDelta)
-            expect(feature[1]).to.be.approximately(coordEpsg3857[1], acceptableDelta)
+            expect(feature[0]).to.be.approximately(expectedCenter[0], acceptableDelta)
+            expect(feature[1]).to.be.approximately(expectedCenter[1], acceptableDelta)
         })
     }
     const numberWithThousandSeparator = (x, separator = "'") => {
@@ -116,7 +136,7 @@ describe('Testing coordinates typing in search bar', { testIsolation: false }, (
         cy.get(searchbarSelector).should('be.visible')
     })
     it('Remove the dropped pin when the search bar is cleared', () => {
-        cy.get(searchbarSelector).paste(`${LV95[0]} ${LV95[1]}`)
+        cy.get(searchbarSelector).paste(`${expectedCenterLV95[0]} ${expectedCenterLV95[1]}`)
         checkCenterInStore()
         checkZoomLevelInStore()
         checkThatCoordinateAreHighlighted()
@@ -133,7 +153,7 @@ describe('Testing coordinates typing in search bar', { testIsolation: false }, (
         const WGS84_DM_GOOGLE_STYLE = ['47 12.6095', '6 57.12372']
         const WGS84_DMS = ['47°12\'36.57"', '6°57\'7.423"']
         const WGS84_DMS_WITH_CARDINAL = ['47°12\'36.57"N', '6°57\'7.423"E']
-        tryAllInputPossibilities(WGS84[0], WGS84[1], 'DD format')
+        tryAllInputPossibilities(centerWGS84[1], centerWGS84[0], 'DD format')
         tryAllInputPossibilities(WGS84_DM[0], WGS84_DM[1], 'DM format')
         tryAllInputPossibilities(
             WGS84_DM_GOOGLE_STYLE[0],
@@ -153,10 +173,24 @@ describe('Testing coordinates typing in search bar', { testIsolation: false }, (
         )
     })
     context('EPSG:2056 (LV95) inputs', () => {
-        tryAllInputPossibilities(LV95[0], LV95[1], 'LV95', 0.0, true, true)
+        tryAllInputPossibilities(
+            expectedCenterLV95[0],
+            expectedCenterLV95[1],
+            'LV95',
+            0.0,
+            true,
+            true
+        )
     })
     context('EPSG:21781 (LV03) inputs', () => {
-        tryAllInputPossibilities(LV03[0], LV03[1], 'LV03', 0.1, true, true)
+        tryAllInputPossibilities(
+            expectedCenterLV03[0],
+            expectedCenterLV03[1],
+            'LV03',
+            0.1,
+            true,
+            true
+        )
     })
     context('What3Words input', () => {
         const what3words = 'bisher.meiste.einerseits'
@@ -164,8 +198,8 @@ describe('Testing coordinates typing in search bar', { testIsolation: false }, (
         const w3wStub = {
             country: 'CH',
             coordinates: {
-                lat: WGS84[0],
-                lng: WGS84[1],
+                lat: centerWGS84[0],
+                lng: centerWGS84[1],
             },
             words: what3words,
             language: 'en',
