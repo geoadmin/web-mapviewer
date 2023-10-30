a71e36aad77e34ba99306a923268ed109cc9749c
ltkum <kunzi.martin@gmail.com>
2023-10-30 13:43:45 +0100

    moving most of the logic inside the CompareSlider class

--- src/modules/map/components/CompareSlider.vue
+++ src/modules/map/components/CompareSlider.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="compare-slider" :style="compareSliderPosition">
+    <div
+        class="compare-slider"
+        :style="compareSliderPosition"
+        @touchstart.passive="grabSlider"
+        @mousedown.passive="grabSlider"
+    >
         <FontAwesomeIcon class="compare-slider-caret-left" :icon="['fas', 'caret-left']" />
         <div class="compare-slider-line"></div>
 
@@ -9,19 +14,71 @@
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
+import debounce from '@/utils/debounce'
 export default {
+    data() {
+        return {
+            compareSliderIsGrabbed: false,
+            clientWidth: window.innerWidth,
+            compareRatio: -0.5,
+        }
+    },
+    inject: ['getMap'],
     components: {
         FontAwesomeIcon,
     },
     computed: {
         ...mapState({
-            compareRatio: (state) => state.ui.compareRatio,
+            storeCompareRatio: (state) => state.ui.compareRatio,
         }),
+        ...mapGetters(['visibleLayers']),
         compareSliderPosition() {
             return {
                 left: this.compareRatio * 100 + '%',
             }
+        },
+    },
+    mounted() {
+        this.compareRatio = this.storeCompareRatio
+    },
+    methods: {
+        ...mapActions(['setCompareRatio']),
+        grabSlider(event) {
+            this.compareSliderIsGrabbed = true
+            if (event.type === 'touchstart') {
+                // for touch events we have to select which touch we want to get the screen position
+                // (there can be multiple fingers gestures)
+                this.cursorX = event.touches[0].screenX
+            } else {
+                this.cursorX = event.screenX
+            }
+            window.addEventListener('mousemove', this.listenToMouseMove, { passive: true })
+            window.addEventListener('touchmove', this.listenToMouseMove, { passive: true })
+            window.addEventListener('mouseup', this.releaseSlider, { passive: true })
+            window.addEventListener('touchend', this.releaseSlider, { passive: true })
+        },
+        listenToMouseMove(event) {
+            const currentPosition =
+                event.type === 'touchmove' ? event.touches[0].screenX : event.screenX
+            const deltaX = this.cursorX - currentPosition
+            // COMMIT to compareRatio
+            console.log(deltaX)
+            console.log((currentPosition + deltaX) / this.clientWidth)
+            this.compareRatio = (currentPosition + deltaX) / this.clientWidth
+        },
+        releaseSlider() {
+            this.compareSliderIsGrabbed = false
+            window.removeEventListener('mousemove', this.listenToMouseMove)
+            window.removeEventListener('touchmove', this.listenToMouseMove)
+            window.removeEventListener('mouseup', this.releaseSlider)
+            window.removeEventListener('touchend', this.releaseSlider)
+            this.setCompareRatio(this.compareRatio)
+        },
+    },
+    watch: {
+        storeCompareRatio() {
+            this.compareRatio = this.storeCompareRatio
         },
     },
 }
