ce9253385621c76ed212211ce708c684ad9c5588
ltkum <kunzi.martin@gmail.com>
2023-10-31 11:44:35 +0100

    Slider is now able to move
    removed useless declarations and variables
    scss adaptation for ticket 3106
    still to do : when sliding on a partial window, this doesn't follow the
    mouse on the screen, but takes a ratio across the window"

--- src/modules/map/components/CompareSlider.vue
+++ src/modules/map/components/CompareSlider.vue
@@ -14,12 +14,10 @@
 
 <script>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
-import debounce from '@/utils/debounce'
+import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
     data() {
         return {
-            compareSliderIsGrabbed: false,
             clientWidth: window.innerWidth,
             compareRatio: -0.5,
         }
@@ -45,14 +43,6 @@ export default {
     methods: {
         ...mapActions(['setCompareRatio']),
         grabSlider(event) {
-            this.compareSliderIsGrabbed = true
-            if (event.type === 'touchstart') {
-                // for touch events we have to select which touch we want to get the screen position
-                // (there can be multiple fingers gestures)
-                this.cursorX = event.touches[0].screenX
-            } else {
-                this.cursorX = event.screenX
-            }
             window.addEventListener('mousemove', this.listenToMouseMove, { passive: true })
             window.addEventListener('touchmove', this.listenToMouseMove, { passive: true })
             window.addEventListener('mouseup', this.releaseSlider, { passive: true })
@@ -60,15 +50,11 @@ export default {
         },
         listenToMouseMove(event) {
             const currentPosition =
-                event.type === 'touchmove' ? event.touches[0].screenX : event.screenX
-            const deltaX = this.cursorX - currentPosition
-            // COMMIT to compareRatio
-            console.log(deltaX)
-            console.log((currentPosition + deltaX) / this.clientWidth)
-            this.compareRatio = (currentPosition + deltaX) / this.clientWidth
+                event.type === 'touchmove' ? event.touches[0].pageX : event.pageX
+            // THIS DIES ON CLIENT SMALLER THAN SCREEN
+            this.compareRatio = currentPosition / this.clientWidth
         },
         releaseSlider() {
-            this.compareSliderIsGrabbed = false
             window.removeEventListener('mousemove', this.listenToMouseMove)
             window.removeEventListener('touchmove', this.listenToMouseMove)
             window.removeEventListener('mouseup', this.releaseSlider)
@@ -93,37 +79,32 @@ export default {
     display: inline-block;
     width: 40px;
     height: 100%;
-    border-top: 10px solid transparent;
-    border-bottom: 10px solid transparent;
     z-index: $zindex-compare-slider;
-}
-.compare-slider-caret-left {
-    position: absolute;
-    translate: 4px;
-    top: 50%;
-    z-index: $zindex-compare-slider;
-    width: 14px;
-    height: 14px;
-    color: white;
-    background: red;
-}
 
-.compare-slider-caret-right {
-    position: absolute;
-    top: 50%;
-    translate: 22px;
-    z-index: $zindex-compare-slider;
-    width: 14px;
-    height: 14px;
-    color: white;
-    background: red;
-}
-.compare-slider-line {
-    position: relative;
-    width: 4px;
-    height: 100%;
-    margin: auto;
-    z-index: $zindex-compare-slider;
-    background-color: red;
+    &-caret-left,
+    &-caret-right {
+        position: inherit;
+        top: 50%;
+        width: 14px;
+        height: 14px;
+        color: $white;
+        background: $primary;
+        z-index: inherit;
+    }
+
+    &-caret-left {
+        translate: 4px;
+    }
+    &-caret-right {
+        translate: 22px;
+    }
+    &-line {
+        position: relative;
+        margin: auto;
+        width: 4px;
+        height: 100%;
+        background: $primary;
+        z-index: inherit;
+    }
 }
 </style>
