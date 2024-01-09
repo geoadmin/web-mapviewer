0de42c432d054c054b7001404ec8988002f8e3a2
Brice Schaffner <brice.schaffner@swisstopo.ch>
2024-01-09 22:30:50 +0100

    PB-73: Set the emphasize text in search of the import external layers with a bg color

--- tests/cypress/tests-component/TextSearchMarker.cy.js
+++ tests/cypress/tests-component/TextSearchMarker.cy.js
@@ -4,7 +4,7 @@ describe('<TextSearchMarker />', () => {
     it('Renders', () => {
         cy.log('It mark a search text in bold')
         cy.mount(TextSearchMarker, {
-            props: { text: 'This a simple text', search: 'simple' },
+            props: { text: 'This a simple text', search: 'simple', markers: 'fw-bold' },
         })
         cy.get('[data-cy="segment"]').each(($el) => {
             expect($el).to.be.visible
@@ -15,9 +15,10 @@ describe('<TextSearchMarker />', () => {
             .should('have.class', 'fw-bold')
             .contains('simple')
 
+        //----------------------------------------------------------------------
         cy.log('It mark a search pattern in bold')
         cy.mount(TextSearchMarker, {
-            props: { text: 'This a simple-345 text', search: /simple-\d+/ },
+            props: { text: 'This a simple-345 text', search: /simple-\d+/, markers: 'fw-bold' },
         })
         cy.get('[data-cy="segment"]').each(($el) => {
             expect($el).to.be.visible
@@ -28,9 +29,14 @@ describe('<TextSearchMarker />', () => {
             .should('have.class', 'fw-bold')
             .contains('simple-345')
 
+        //----------------------------------------------------------------------
         cy.log('It mark multiple search pattern in bold')
         cy.mount(TextSearchMarker, {
-            props: { text: 'This a simple-345 text and simple-100 text', search: /simple-\d+/ },
+            props: {
+                text: 'This a simple-345 text and simple-100 text',
+                search: /simple-\d+/,
+                markers: 'fw-bold',
+            },
         })
         cy.get('[data-cy="segment"]').each(($el) => {
             expect($el).to.be.visible
@@ -51,5 +57,55 @@ describe('<TextSearchMarker />', () => {
                     break
             }
         })
+
+        //----------------------------------------------------------------------
+        cy.log('It mark a search pattern with default markers')
+        cy.mount(TextSearchMarker, {
+            props: { text: 'This a simple-345 text', search: /simple-\d+/ },
+        })
+        cy.get('[data-cy="segment"]').each(($el) => {
+            expect($el).to.be.visible
+            expect($el).not.to.have.class('fw-bold')
+        })
+        cy.get('[data-cy="segment-match"]')
+            .should('be.visible')
+            .should('have.class', 'fw-bold bg-info bg-opacity-25')
+            .contains('simple-345')
+
+        //----------------------------------------------------------------------
+        cy.log('It mark a search pattern with multiple markers as string')
+        cy.mount(TextSearchMarker, {
+            props: {
+                text: 'This a simple-345 text',
+                search: /simple-\d+/,
+                markers: 'fw-bold bg-secondary',
+            },
+        })
+        cy.get('[data-cy="segment"]').each(($el) => {
+            expect($el).to.be.visible
+            expect($el).not.to.have.class('fw-bold')
+        })
+        cy.get('[data-cy="segment-match"]')
+            .should('be.visible')
+            .should('have.class', 'fw-bold bg-secondary')
+            .contains('simple-345')
+
+        //----------------------------------------------------------------------
+        cy.log('It mark a search pattern with multiple markers as list')
+        cy.mount(TextSearchMarker, {
+            props: {
+                text: 'This a simple-345 text',
+                search: /simple-\d+/,
+                markers: ['fw-bold', 'bg-secondary'],
+            },
+        })
+        cy.get('[data-cy="segment"]').each(($el) => {
+            expect($el).to.be.visible
+            expect($el).not.to.have.class('fw-bold')
+        })
+        cy.get('[data-cy="segment-match"]')
+            .should('be.visible')
+            .should('have.class', 'fw-bold bg-secondary')
+            .contains('simple-345')
     })
 })
