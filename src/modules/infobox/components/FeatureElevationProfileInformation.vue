79ad91f2d9f9f4673f2a0542f4dabc811807d330
Lukas Joss <lukas.joss@swisstopo.ch>
2024-03-05 08:49:44 +0100

    PB-74: Allow text wrap for profile information in mobile

--- src/modules/infobox/components/FeatureElevationProfileInformation.vue
+++ src/modules/infobox/components/FeatureElevationProfileInformation.vue
@@ -2,7 +2,7 @@
     <div class="d-flex p-1 profile-popup-info-buttons">
         <div
             v-if="profile && profile.hasElevationData"
-            class="flex-grow-1 profile-info-container d-flex border border-light ps-1 pe-4 py-1"
+            class="flex-grow-1 profile-info-container d-flex flex-wrap border border-light ps-1 pe-4 py-1"
             data-cy="profile-popup-info-container"
         >
             <small
