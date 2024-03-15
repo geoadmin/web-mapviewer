d6de8ed43db682a0d98590bbcbde5c7fbd4cd9d6
Lukas Joss <lukas.joss@swisstopo.ch>
2024-03-15 16:56:06 +0100

    PB-271: Allow input to be passed to description

--- src/modules/infobox/components/styling/DrawingStyleMedia.vue
+++ src/modules/infobox/components/styling/DrawingStyleMedia.vue
@@ -1,37 +1,73 @@
 <script setup>
-import { toRefs } from 'vue'
+import { defineEmits, ref, toRefs } from 'vue'
+
+const descriptionMediaLink = ref('')
 
 const props = defineProps({
-    value: {
+    descriptionTitle: {
         type: String,
         required: true,
     },
-    extraValue: {
+    extraDescriptionTitle: {
         type: String,
         default: null,
     },
 })
-const { value, extraValue } = toRefs(props)
+
+const { descriptionTitle, extraDescriptionTitle } = toRefs(props)
+const emit = defineEmits(['descriptionMediaLink'])
+
+function addLink(descriptionMediaLink) {
+    emit('descriptionMediaLink', descriptionMediaLink)
+}
 </script>
 
 <template>
-    <div>
-        <label class="form-label" for="drawing-style-feature-description"> {{ value }} </label>
-        <textarea
-            id="drawing-style-feature-description"
-            data-cy="drawing-style-media-description"
-            class="feature-description form-control"
-        ></textarea>
+    <label class="form-label" for="drawing-style-feature-descriptionTitle">
+        {{ descriptionTitle }}
+    </label>
+    <div class="input-group d-flex needs-validation">
+        <input
+            id="drawing-style-feature-descriptionTitle"
+            ref="inputElement"
+            v-model="descriptionMediaLink"
+            type="text"
+            data-cy="drawing-style-media-descriptionTitle"
+            class="feature-descriptionTitle form-control"
+        />
+        <button
+            v-if="descriptionTitle?.length > 0"
+            class="btn btn-outline-group rounded-end"
+            type="button"
+            data-cy="text-input-clear"
+            @click="addLink(descriptionMediaLink)"
+        >
+            <FontAwesomeIcon :icon="['fas', 'times-circle']" />
+        </button>
     </div>
-    <div v-if="extraValue" class="pt-2">
-        <label class="form-label" for="drawing-style-feature-description">
-            {{ extraValue }}
+
+    <div v-if="extraDescriptionTitle" class="pt-2">
+        <label class="form-label" for="drawing-style-feature-descriptionTitle">
+            {{ extraDescriptionTitle }}
         </label>
-        <textarea
-            id="drawing-style-feature-description"
-            data-cy="drawing-style-media-description"
-            class="feature-description form-control"
-        ></textarea>
+        <div class="input-group d-flex needs-validation">
+            <input
+                id="drawing-style-feature-descriptionTitle"
+                ref="inputElement"
+                type="text"
+                data-cy="drawing-style-media-descriptionTitle"
+                class="feature-descriptionTitle form-control"
+            />
+            <button
+                v-if="descriptionTitle?.length > 0"
+                class="btn btn-outline-group rounded-end"
+                type="button"
+                data-cy="text-input-clear"
+                @click="addLink(descriptionMediaLink)"
+            >
+                <FontAwesomeIcon :icon="['fas', 'times-circle']" />
+            </button>
+        </div>
     </div>
 </template>
 
