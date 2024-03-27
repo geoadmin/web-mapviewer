448f9c9510a7aa7b7afd607bd87097b59487c1f1
Pascal Barth <pak.barth@gmail.com>
2024-03-27 18:06:45 +0100

    PB-368 : add rectangle feature on map after a CTRL drag select

---
+++ src/modules/map/components/openlayers/OpenLayersRectangleSelectionFeedback.vue
@@ -0,0 +1,51 @@
+<script setup>
+import Feature from 'ol/Feature'
+import { Polygon } from 'ol/geom'
+import VectorLayer from 'ol/layer/Vector'
+import VectorSource from 'ol/source/Vector'
+import { computed, inject, watch } from 'vue'
+import { useStore } from 'vuex'
+
+import { selectionBoxStyle } from '@/utils/styleUtils'
+
+const store = useStore()
+const lastClick = computed(() => store.state.map.clickInfo)
+const selectionPolygon = computed(() => {
+    if (lastClick.value?.coordinate.length === 4) {
+        const bottomLeft = [lastClick.value.coordinate[0], lastClick.value.coordinate[1]]
+        const bottomRight = [lastClick.value.coordinate[2], lastClick.value.coordinate[1]]
+        const topRight = [lastClick.value.coordinate[2], lastClick.value.coordinate[3]]
+        const topLeft = [lastClick.value.coordinate[0], lastClick.value.coordinate[3]]
+        return new Feature({
+            geometry: new Polygon([[bottomLeft, bottomRight, topRight, topLeft, bottomLeft]]),
+        })
+    }
+    return null
+})
+
+// Create a VectorLayer to show the selection feedback
+const map = inject('olMap')
+const vectorLayer = new VectorLayer({
+    id: 'rectangleSelectionFeedback',
+    source: new VectorSource({
+        features: [],
+    }),
+    style: selectionBoxStyle,
+})
+
+watch(selectionPolygon, () => updateLayer())
+
+function updateLayer() {
+    vectorLayer.getSource().clear()
+    if (selectionPolygon.value) {
+        vectorLayer.getSource().addFeature(selectionPolygon.value)
+        map.addLayer(vectorLayer)
+    } else {
+        map.removeLayer(vectorLayer)
+    }
+}
+</script>
+
+<template>
+    <slot />
+</template>
