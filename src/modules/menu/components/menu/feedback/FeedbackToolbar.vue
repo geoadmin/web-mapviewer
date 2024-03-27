87e9bb3fb754b0176686713201142b3db5488611
Ismail Sunni <imajimatika@gmail.com>
2024-03-27 14:47:33 +0700

    PB-239: Convert FeedbackToolbar to composition API.

--- src/modules/menu/components/menu/feedback/FeedbackToolbar.vue
+++ src/modules/menu/components/menu/feedback/FeedbackToolbar.vue
@@ -1,8 +1,29 @@
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+import FeedbackButton from '@/modules/menu/components/menu/feedback/FeedbackButton.vue'
+import MoreInfo from '@/modules/menu/components/menu/MoreInfo.vue'
+import ReportProblemButton from '@/modules/menu/components/menu/reportProblem/ReportProblemButton.vue'
+
+const props = defineProps({
+    showAsLinks: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+const store = useStore()
+
+const hasGiveFeedbackButton = computed(() => store.getters.hasGiveFeedbackButton)
+const hasReportProblemButton = computed(() => store.getters.hasReportProblemButton)
+</script>
+
 <template>
     <div
         :class="{
-            'feedback-menu p-1': !showAsLinks,
-            'me-2': showAsLinks,
+            'feedback-menu p-1': !props.showAsLinks,
+            'me-2': props.showAsLinks,
         }"
     >
         <FeedbackButton v-if="hasGiveFeedbackButton" :show-as-link="showAsLinks" />
@@ -10,28 +31,3 @@
         <MoreInfo :show-as-link="showAsLinks" />
     </div>
 </template>
-
-<script>
-import { mapGetters } from 'vuex'
-
-import FeedbackButton from '@/modules/menu/components/menu/feedback/FeedbackButton.vue'
-import MoreInfo from '@/modules/menu/components/menu/MoreInfo.vue'
-import ReportProblemButton from '@/modules/menu/components/menu/reportProblem/ReportProblemButton.vue'
-
-export default {
-    components: {
-        FeedbackButton,
-        MoreInfo,
-        ReportProblemButton,
-    },
-    props: {
-        showAsLinks: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    computed: {
-        ...mapGetters(['hasGiveFeedbackButton', 'hasReportProblemButton']),
-    },
-}
-</script>
