d836bed380f11fec68e66970d540d7c955279841
Pascal Barth <pak.barth@gmail.com>
2024-03-27 10:45:33 +0100

    PR Review
    move geolocation removal into API file directly (so that the logic is shared across the app
    use store value directly when printing, no need to go through a reactivity computed prop as we won't be reacting to changes while printing

--- src/modules/map/components/openlayers/utils/usePrint.composable.js
+++ src/modules/map/components/openlayers/utils/usePrint.composable.js
@@ -1,11 +1,12 @@
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { useStore } from 'vuex'
 
 import { abortPrintJob, createPrintJob, waitForPrintJobCompletion } from '@/api/print.api.js'
 import { getGenerateQRCodeUrl } from '@/api/qrcode.api.js'
+import { createShortLink } from '@/api/shortlink.api.js'
 import log from '@/utils/logging'
 
-const dispatcher = { dispatcher: 'usePrint.composable' }
+// const dispatcher = { dispatcher: 'usePrint.composable' }
 
 /** @enum */
 export const PrintStatus = {
@@ -26,19 +27,6 @@ export function usePrint(map) {
     const printStatus = ref(PrintStatus.IDLE)
 
     const store = useStore()
-    const printLayout = computed(() => store.state.print.selectedLayout)
-    const printScale = computed(() => store.state.print.selectedScale)
-    const attributions = computed(() =>
-        store.getters.visibleLayers
-            .map((layer) => layer.attributions)
-            .map((attribution) => attribution.name)
-            .filter((attribution, index, self) => self.indexOf(attribution) === index)
-    )
-    const layersWithLegends = computed(() =>
-        store.getters.visibleLayers.filter((layer) => layer.hasLegend)
-    )
-    const lang = computed(() => store.state.i18n.lang)
-    const projection = computed(() => store.state.map.projection)
 
     /**
      * @param {Boolean} printGrid Print the coordinate grid on the finished PDF, true or false
@@ -48,23 +36,30 @@ export function usePrint(map) {
      */
     async function print(printGrid = false, printLegend = false) {
         try {
+            // TODO PB-362 : show laoding bar
             if (currentJobReference.value) {
                 await abortCurrentJob()
             }
             printStatus.value = PrintStatus.PRINTING
-            await store.dispatch('generateShortLinks', dispatcher)
-            const shortLink = store.state.share.shortLink
+            const shortLink = await createShortLink(window.location.href)
             const qrCodeUrl = getGenerateQRCodeUrl(shortLink)
+            // using store values directly (instead of going through computed) so that it is a bit more performant
+            // (we do not need to have reactivity on these values, if they change while printing we do nothing)
             const printJob = await createPrintJob(map, {
-                layout: printLayout.value,
-                scale: printScale.value,
-                attributions: attributions.value,
+                layout: store.state.print.selectedLayout,
+                scale: store.state.print.selectedScale,
+                attributions: store.getters.visibleLayers
+                    .map((layer) => layer.attributions)
+                    .map((attribution) => attribution.name)
+                    .filter((attribution, index, self) => self.indexOf(attribution) === index),
                 qrCodeUrl,
                 shortLink,
-                layersWithLegends: printLegend ? layersWithLegends.value : [],
-                lang: lang.value,
+                layersWithLegends: printLegend
+                    ? store.getters.visibleLayers.filter((layer) => layer.hasLegend)
+                    : [],
+                lang: store.state.i18n.lang,
                 printGrid: printGrid,
-                projection: projection.value,
+                projection: store.state.map.projection,
             })
             currentJobReference.value = printJob.ref
             const result = await waitForPrintJobCompletion(printJob)
@@ -75,6 +70,7 @@ export function usePrint(map) {
             printStatus.value = PrintStatus.FINISHED_FAILED
             return null
         } finally {
+            // TODO PB-362 : hide laoding bar
             currentJobReference.value = null
         }
     }
