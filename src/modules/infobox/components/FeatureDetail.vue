b226bc392d12fa6b2f21def624f28a4f3e051e71
Lukas Joss <lukas.joss@swisstopo.ch>
2024-03-28 17:36:33 +0100

    PB-271: part to party

--- src/modules/infobox/components/FeatureDetail.vue
+++ src/modules/infobox/components/FeatureDetail.vue
@@ -8,7 +8,7 @@ import { useStore } from 'vuex'
 import SelectableFeature from '@/api/features/SelectableFeature.class.js'
 import FeatureAreaInfo from '@/modules/infobox/components/FeatureAreaInfo.vue'
 import CoordinateCopySlot from '@/utils/components/CoordinateCopySlot.vue'
-import ThirdPartDisclaimer from '@/utils/components/ThirdPartDisclaimer.vue'
+import ThirdPartyDisclaimer from '@/utils/components/ThirdPartyDisclaimer.vue'
 import allFormats from '@/utils/coordinates/coordinateFormat'
 
 const dispatcher = { dispatcher: 'FeatureDetail.vue' }
@@ -97,7 +97,7 @@ function setDisclaimerAgree() {
                 >
                     <div class="d-flex justify-content-between">
                         <div class="d-flex align-items-center">
-                            <ThirdPartDisclaimer
+                            <ThirdPartyDisclaimer
                                 :complete-disclaimer-on-click="true"
                                 :source-name="iframeLinks(value)"
                             >
@@ -112,7 +112,7 @@ function setDisclaimerAgree() {
                                         data-cy="menu-external-disclaimer-icon"
                                     />
                                 </button>
-                            </ThirdPartDisclaimer>
+                            </ThirdPartyDisclaimer>
                             <span class="url-tooltip">
                                 <div
                                     ref="shareTabButton"
