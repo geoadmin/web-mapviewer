d49be8e619a34c0d8aa365fa6b4d5b2db9c326a3
Lukas Joss <lukas.joss@swisstopo.ch>
2024-04-08 11:53:57 +0200

    PB-271: Add more specific checks to kml updates

--- tests/cypress/tests-e2e/drawing.cy.js
+++ tests/cypress/tests-e2e/drawing.cy.js
@@ -249,11 +249,12 @@ describe('Drawing module tests', () => {
                     })
                 })
             })
-
             cy.log('Coordinates for marker can be copied in drawing mode')
             cy.clickDrawingTool(EditableFeatureTypes.MARKER)
             cy.get('[data-cy="ol-map"]').click(160, 200)
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){4}`)]))
             readCoordinateClipboard(
                 'feature-style-edit-coordinate-copy',
                 "2'660'013.50, 1'227'172.00"
@@ -317,6 +318,8 @@ describe('Drawing module tests', () => {
                 `<a target="_blank" href="${valid_url}">${media_description}</a>`
             )
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){6}`)]))
             cy.get('[data-cy="infobox-close"]').click()
 
             cy.log('Open image embed popup')
@@ -335,6 +338,8 @@ describe('Drawing module tests', () => {
                 `<image src="${valid_url}" style="max-height:200px;"/>`
             )
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){8}`)]))
             cy.get('[data-cy="infobox-close"]').click()
 
             cy.log('Open video embed popup')
@@ -353,6 +358,8 @@ describe('Drawing module tests', () => {
                 `<iframe src="${valid_url}" height="200" width="auto"></iframe>`
             )
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){10}`)]))
             cy.get('[data-cy="infobox-close"]').click()
 
             cy.clickDrawingTool(EditableFeatureTypes.MARKER)
@@ -363,6 +370,8 @@ describe('Drawing module tests', () => {
             cy.get('[data-cy="drawing-style-media-url-input"]').type(valid_url)
             cy.get('[data-cy="drawing-style-media-generate-button"]').click()
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){12}`)]))
             cy.get('[data-cy="infobox-close"]').click()
 
             cy.clickDrawingTool(EditableFeatureTypes.MARKER)
@@ -373,6 +382,8 @@ describe('Drawing module tests', () => {
             cy.get('[data-cy="drawing-style-media-url-input"]').type(valid_whitelisted_url)
             cy.get('[data-cy="drawing-style-media-generate-button"]').click()
             cy.wait('@update-kml')
+                .its('request')
+                .should((request) => checkKMLRequest(request, [new RegExp(`(ExtendedData.*){14}`)]))
             cy.get('[data-cy="infobox-close"]').click()
 
             cy.closeDrawingMode()
