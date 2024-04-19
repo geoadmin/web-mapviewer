6f4b034a2445db1fec6277f36397cee7478d07ef
Brice Schaffner <brice.schaffner@swisstopo.ch>
2024-04-19 06:40:36 +0200

    PB-389: Fix print attribution list
    The attribution (copyright) on the pdf was empty.
    
    Also add the background layer attribution if any background.

--- src/modules/map/components/openlayers/utils/usePrint.composable.js
+++ src/modules/map/components/openlayers/utils/usePrint.composable.js
@@ -61,7 +61,10 @@ export function usePrint(map) {
                 layout: store.state.print.selectedLayout,
                 scale: store.state.print.selectedScale,
                 attributions: store.getters.visibleLayers
+                    .concat([store.state.layers.currentBackgroundLayer])
+                    .filter((layer) => !!layer)
                     .map((layer) => layer.attributions)
+                    .flat()
                     .map((attribution) => attribution.name)
                     .filter((attribution, index, self) => self.indexOf(attribution) === index),
                 qrCodeUrl,
