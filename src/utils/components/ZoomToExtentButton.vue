115edf67875955d498f8fcd15c1380c43baeb782
Pascal Barth <pak.barth@gmail.com>
2024-05-13 11:05:03 +0200

    PB-368 : fix zoom to extent tooltip being applied too many times
    the selector for the zoom to extent Tippy tooltip was too eager and was re-applying the same tooltip over and over if more than one zoomToExtent button was present at a given time.
    Adding a component counter to give each component instance a unique ID (and applying Tippy only on this unique ID)

--- src/utils/components/ZoomToExtentButton.vue
+++ src/utils/components/ZoomToExtentButton.vue
@@ -1,6 +1,19 @@
+<script>
+/**
+ * As we use a Tippy tooltip for this button, if we do not use a unique selector all the button
+ * rendered in a feature list will be applied, and re-applied (as many time as there are zoom to
+ * extent button) leading to a huge stacking of tippy tooltip at each button.
+ *
+ * Using here a button counter to give each a unique ID (and have a unique Tippy selector)
+ *
+ * @type {number}
+ */
+let zoomToExtentButtonCount = 0
+</script>
+
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { toRefs } from 'vue'
+import { ref, toRefs } from 'vue'
 import { useStore } from 'vuex'
 
 import { useTippyTooltip } from '@/utils/composables/useTippyTooltip'
@@ -18,7 +31,9 @@ const props = defineProps({
 
 const { extent } = toRefs(props)
 
-useTippyTooltip('.zoom-to-extent-button', { placement: 'top' })
+const uniqueId = ref(`zoom-to-extent-button-${++zoomToExtentButtonCount}`)
+
+useTippyTooltip(`#${uniqueId.value}[data-tippy-content]`, { placement: 'top' })
 
 const store = useStore()
 
@@ -32,6 +47,7 @@ function zoomToFeatureExtent() {
 
 <template>
     <button
+        :id="uniqueId"
         class="zoom-to-extent-button btn btn-xs text-secondary"
         data-tippy-content="offline_zoom_extent"
         data-cy="zoom-to-feature-extent"
