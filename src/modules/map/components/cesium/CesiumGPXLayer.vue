7fdb2d314b6be3c88e830bed9e44bef0947fb401
Ismail Sunni <imajimatika@gmail.com>
2024-05-30 12:39:27 +0700

    PB-497: Use cesium standard to load GPX (still static).

--- src/modules/map/components/cesium/CesiumGPXLayer.vue
+++ src/modules/map/components/cesium/CesiumGPXLayer.vue
@@ -5,27 +5,30 @@
 </template>
 
 <script>
-import VectorSource from 'ol/source/Vector'
-import { mapState } from 'vuex'
+import { GpxDataSource } from 'cesium'
 
 import GPXLayer from '@/api/layers/GPXLayer.class'
-import { parseGpx } from '@/utils/gpxUtils'
-
-import addPrimitiveFromOLLayerMixins from './utils/addPrimitiveFromOLLayer.mixins'
+import log from '@/utils/logging'
 
 /** Adds a GPX layer to the Cesium viewer */
 export default {
-    mixins: [addPrimitiveFromOLLayerMixins],
+    inject: ['getViewer'],
+
     props: {
         gpxLayerConfig: {
             type: GPXLayer,
             required: true,
         },
     },
+
+    data() {
+        return {
+            isPresentOnMap: false,
+            gpxDataSource: null,
+        }
+    },
+
     computed: {
-        ...mapState({
-            projection: (state) => state.position.projection,
-        }),
         opacity() {
             return this.gpxLayerConfig.opacity
         },
@@ -33,26 +36,54 @@ export default {
             return this.gpxLayerConfig.gpxData
         },
     },
+
     watch: {
         gpxData() {
-            this.loadDataInOLLayer().then(this.addPrimitive)
+            this.addLayer()
         },
     },
+
+    mounted() {
+        log.debug('Mounted GPX layer')
+        const gpxUrl =
+            'https://gist.githubusercontent.com/ismailsunni/e4345dd852c5deaff3434eadefdb467f/raw/0eac92b199e10c6498f4a1c305098a3e74ce1521/map.geo.admin.ch_GPX_20240529033356.gpx'
+        GpxDataSource.load(gpxUrl, {
+            clampToGround: true,
+        }).then((dataSource) => {
+            this.gpxDataSource = dataSource
+            if (!this.isPresentOnMap) {
+                this.addLayer()
+            }
+        })
+    },
+
+    unmounted() {
+        log.debug('Unmounted GPX layer')
+        if (this.gpxDataSource && this.isPresentOnMap) {
+            this.removeLayer()
+        }
+
+        delete this.gpxDataSource
+    },
+
     methods: {
-        loadDataInOLLayer() {
-            return new Promise((resolve, reject) => {
-                if (!this.gpxData) {
-                    reject(new Error('no GPX data loaded yet, could not create source'))
-                }
-                this.olLayer.setSource(
-                    new VectorSource({
-                        wrapX: true,
-                        projection: this.projection,
-                        features: parseGpx(this.gpxData, this.projection),
-                    })
-                )
-                resolve()
-            })
+        addLayer() {
+            log.debug('Adding GPX layer')
+            log.debug(this.gpxDataSource)
+            this.getViewer().dataSources.add(this.gpxDataSource)
+            this.isPresentOnMap = true
+            log.debug(this.getViewer().dataSources)
+        },
+        removeLayer() {
+            log.debug('Remove GPX layer')
+            log.debug(this.gpxDataSource)
+            if (this.gpxDataSource) {
+                this.getViewer().dataSources.remove(this.gpxDataSource)
+                this.gpxDataSource = null
+                this.getViewer().scene.requestRender() // Request a render after removing the DataSource
+            }
+            this.isPresentOnMap = false
+            log.debug(this.getViewer().dataSources)
         },
     },
 }
