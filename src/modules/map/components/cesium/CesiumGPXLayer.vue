06c6975443e8f8fe5a6357cb09f9f8e5384ad261
Ismail Sunni <imajimatika@gmail.com>
2024-06-03 12:20:33 +0700

    PB-497: Convert to composition API with setup script

--- src/modules/map/components/cesium/CesiumGPXLayer.vue
+++ src/modules/map/components/cesium/CesiumGPXLayer.vue
@@ -1,10 +1,4 @@
-<template>
-    <div>
-        <slot />
-    </div>
-</template>
-
-<script>
+<script setup>
 import {
     BillboardGraphics,
     Cartesian3,
@@ -14,143 +8,132 @@ import {
     GpxDataSource,
     HeightReference,
 } from 'cesium'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
+import { inject } from 'vue'
 
 import GPXLayer from '@/api/layers/GPXLayer.class'
 import log from '@/utils/logging'
 
-/** Adds a GPX layer to the Cesium viewer */
-export default {
-    inject: ['getViewer'],
-
-    props: {
-        gpxLayerConfig: {
-            type: GPXLayer,
-            required: true,
-        },
+const props = defineProps({
+    gpxLayerConfig: {
+        type: GPXLayer,
+        required: true,
     },
+})
 
-    data() {
-        return {
-            isPresentOnMap: false,
-            gpxDataSource: new GpxDataSource(),
-        }
-    },
+const isPresentOnMap = ref(false)
+const gpxDataSource = ref(new GpxDataSource())
 
-    computed: {
-        opacity() {
-            return this.gpxLayerConfig.opacity
-        },
-        gpxData() {
-            return this.gpxLayerConfig.gpxData
-        },
-    },
+// const opacity = ref(props.gpxLayerConfig.opacity)
+const gpxData = ref(props.gpxLayerConfig.gpxData)
 
-    watch: {
-        gpxData() {
-            this.addLayer()
-        },
-    },
+const getViewer = inject('getViewer')
+watch(gpxData, () => {
+    addLayer()
+})
 
-    mounted() {
-        log.debug('Mounted GPX layer')
-        this.addLayer()
-    },
+onMounted(() => {
+    log.debug('Mounted GPX layer')
+    addLayer()
+})
 
-    unmounted() {
-        log.debug('Unmounted GPX layer')
-        if (this.gpxDataSource && this.isPresentOnMap) {
-            this.removeLayer()
-        }
+onUnmounted(() => {
+    log.debug('Unmounted GPX layer')
+    if (gpxDataSource.value && isPresentOnMap.value) {
+        removeLayer()
+    }
 
-        delete this.gpxDataSource
-    },
+    gpxDataSource.value = null
+})
 
-    methods: {
-        addLayer() {
-            const gpxBlob = new Blob([this.gpxData], { type: 'application/gpx+xml' })
-            const gpxUrl = URL.createObjectURL(gpxBlob)
+function addLayer() {
+    const gpxBlob = new Blob([gpxData.value], { type: 'application/gpx+xml' })
+    const gpxUrl = URL.createObjectURL(gpxBlob)
 
-            this.gpxDataSource
-                .load(gpxUrl, {
-                    clampToGround: true,
+    gpxDataSource.value
+        .load(gpxUrl, {
+            clampToGround: true,
+        })
+        .then((dataSource) => {
+            getViewer().dataSources.add(dataSource)
+            isPresentOnMap.value = true
+        })
+        .then(updateStyle)
+}
+
+function removeLayer() {
+    log.debug('Remove GPX layer')
+    if (gpxDataSource.value) {
+        getViewer().dataSources.remove(gpxDataSource.value)
+        gpxDataSource.value = null
+        getViewer().scene.requestRender() // Request a render after removing the DataSource
+    }
+    isPresentOnMap.value = false
+}
+
+function updateStyle() {
+    // Function to create a red circle image using a canvas
+    function createRedCircleImage(radius) {
+        // Create a new canvas element
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+
+        // Set the canvas sizes
+        canvas.width = radius * 2
+        canvas.height = radius * 2
+
+        // Draw a red circle on the canvas
+        context.beginPath()
+        context.arc(radius, radius, radius, 0, 2 * Math.PI, false)
+        context.fillStyle = 'red'
+        context.fill()
+
+        // Return the data URL of the canvas drawing
+        return canvas.toDataURL()
+    }
+
+    // Create a red circle image with a radius of 8 pixels
+    const radius = 8
+    const billboardSize = radius * 2
+    const redCircleImage = createRedCircleImage(radius)
+    const entities = gpxDataSource.value._entityCollection.values
+
+    for (let i = 0; i < entities.length; i++) {
+        const entity = entities[i]
+        // Hide the billboard for billboard on the lines by checking if there is a description
+        // Imported GPX files from web-mapviewer have a description for the waypoints
+        // This might be not working for generic GPX files
+        if (cesiumDefined(entity.billboard)) {
+            if (!entity.description) {
+                entity.show = false
+            } else {
+                entity.show = true
+                entity.billboard = new BillboardGraphics({
+                    image: redCircleImage,
+                    width: billboardSize,
+                    height: billboardSize,
+                    eyeOffset: new Cartesian3(0, 0, -100), // Make sure the billboard is always seen
+                    heightReference: HeightReference.CLAMP_TO_TERRAIN, // Make the billboard always appear on top of the terrain
                 })
-                .then((dataSource) => {
-                    this.getViewer().dataSources.add(dataSource)
-                    this.isPresentOnMap = true
-                })
-                .then(() => this.updateStyle())
-        },
-        removeLayer() {
-            log.debug('Remove GPX layer')
-            if (this.gpxDataSource) {
-                this.getViewer().dataSources.remove(this.gpxDataSource)
-                this.gpxDataSource = null
-                this.getViewer().scene.requestRender() // Request a render after removing the DataSource
             }
-            this.isPresentOnMap = false
-        },
-        updateStyle() {
-            // Function to create a red circle image using a canvas
-            function createRedCircleImage(radius) {
-                // Create a new canvas element
-                const canvas = document.createElement('canvas')
-                const context = canvas.getContext('2d')
+        }
 
-                // Set the canvas sizes
-                canvas.width = radius * 2
-                canvas.height = radius * 2
+        if (cesiumDefined(entity.polyline)) {
+            entity.polyline.material = new ColorMaterialProperty(Color.RED)
+            entity.polyline.width = 1.5
+        }
 
-                // Draw a red circle on the canvas
-                context.beginPath()
-                context.arc(radius, radius, radius, 0, 2 * Math.PI, false)
-                context.fillStyle = 'red'
-                context.fill()
-
-                // Return the data URL of the canvas drawing
-                return canvas.toDataURL()
-            }
-
-            // Create a red circle image with a radius of 8 pixels
-            const radius = 8
-            const billboardSize = radius * 2
-            const redCircleImage = createRedCircleImage(radius)
-
-            const entities = this.gpxDataSource.entities.values
-            log.debug('Entities:', entities.length)
-            window.entities = entities
-            for (let i = 0; i < entities.length; i++) {
-                const entity = entities[i]
-                // Hide the billboard for billboard on the lines by checking if there is a description
-                // Imported GPX files from web-mapviewer have a description for the waypoints
-                // This might be not working for generic GPX files
-                if (cesiumDefined(entity.billboard)) {
-                    if (!entity.description) {
-                        entity.show = false
-                    } else {
-                        entity.show = true
-                        entity.billboard = new BillboardGraphics({
-                            image: redCircleImage,
-                            width: billboardSize,
-                            height: billboardSize,
-                            eyeOffset: new Cartesian3(0, 0, -100), // Make sure the billboard is always seen
-                            heightReference: HeightReference.CLAMP_TO_TERRAIN, // Make the billboard always appear on top of the terrain
-                        })
-                    }
-                }
-
-                if (cesiumDefined(entity.polyline)) {
-                    entity.polyline.material = new ColorMaterialProperty(Color.RED)
-                    entity.polyline.width = 1.5
-                }
-
-                if (cesiumDefined(entity.polygon)) {
-                    entity.polygon.material = new ColorMaterialProperty(Color.RED)
-                    entity.polygon.outline = true
-                    entity.polygon.outlineColor = Color.BLACK
-                }
-            }
-            this.getViewer().scene.requestRender()
-        },
-    },
+        if (cesiumDefined(entity.polygon)) {
+            entity.polygon.material = new ColorMaterialProperty(Color.RED)
+            entity.polygon.outline = true
+            entity.polygon.outlineColor = Color.BLACK
+        }
+    }
+    getViewer().scene.requestRender()
 }
 </script>
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
