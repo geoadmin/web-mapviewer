fdc288590ad8a131c91e5950341131c9230ff367
Pascal Barth <pak.barth@gmail.com>
2024-06-28 15:58:51 +0200

    PB-170 : adding a cone to show where the device is pointing (compass)

--- src/modules/map/components/openlayers/OpenLayersGeolocationFeedback.vue
+++ src/modules/map/components/openlayers/OpenLayersGeolocationFeedback.vue
@@ -5,6 +5,7 @@ import { useStore } from 'vuex'
 import { useLayerZIndexCalculation } from '@/modules/map/components/common/z-index.composable'
 import OpenLayersAccuracyCircle from '@/modules/map/components/openlayers/OpenLayersAccuracyCircle.vue'
 import OpenLayersMarker from '@/modules/map/components/openlayers/OpenLayersMarker.vue'
+import OpenLayersVisionCone from '@/modules/map/components/openlayers/OpenLayersVisionCone.vue'
 import { OpenLayersMarkerStyles } from '@/modules/map/components/openlayers/utils/markerStyle'
 
 const store = useStore()
@@ -17,10 +18,11 @@ const { zIndexGeolocation } = useLayerZIndexCalculation()
 <template>
     <!-- Adding marker and accuracy circle for Geolocation -->
     <OpenLayersAccuracyCircle v-if="geolocationActive" :z-index="zIndexGeolocation" />
+    <OpenLayersVisionCone v-if="geolocationActive" :z-index="zIndexGeolocation + 1" />
     <OpenLayersMarker
         v-if="geolocationActive"
         :position="geolocationPosition"
         :marker-style="OpenLayersMarkerStyles.POSITION"
-        :z-index="zIndexGeolocation + 1"
+        :z-index="zIndexGeolocation + 2"
     />
 </template>
