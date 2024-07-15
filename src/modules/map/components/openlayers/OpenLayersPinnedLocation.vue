3561f018c963c522ebf5116d23e2b2c24b0c8d33
Ismail Sunni <imajimatika@gmail.com>
2024-07-15 13:48:41 +0700

    PB-773: Add circle to highlight the pin when selected.

--- src/modules/map/components/openlayers/OpenLayersPinnedLocation.vue
+++ src/modules/map/components/openlayers/OpenLayersPinnedLocation.vue
@@ -12,15 +12,6 @@ const previewedPinnedLocation = computed(() => store.state.map.previewedPinnedLo
 const { zIndexDroppedPin, zIndexPreviewPosition } = useLayerZIndexCalculation()
 
 const pinnedLocationSelected = ref(false)
-const markerStyle = computed(() => {
-    if (pinnedLocationSelected.value) {
-        // TODO(IS): Find a better way to highlight selected pinned location
-        return 'circle'
-    } else {
-        return 'balloon'
-    }
-})
-
 function selectFeatureCallback(feature) {
     console.log('Custom callback feature selected:', feature)
     pinnedLocationSelected.value = true
@@ -36,10 +27,18 @@ function deselectFeatureCallback() {
     <OpenLayersMarker
         v-if="pinnedLocation"
         :position="pinnedLocation"
-        :marker-style="markerStyle"
+        :marker-style="'balloon'"
         :z-index="zIndexDroppedPin"
         :select-feature-callback="selectFeatureCallback"
         :deselect-feature-callback="deselectFeatureCallback"
+    />
+    <!-- For highlighting the pinned location -->
+    <!-- TODO: do a better way to highlighting it -->
+    <OpenLayersMarker
+        v-if="pinnedLocation && pinnedLocationSelected"
+        :position="pinnedLocation"
+        :marker-style="'circle'"
+        :z-index="zIndexDroppedPin"
     />
     <OpenLayersMarker
         v-if="previewedPinnedLocation"
