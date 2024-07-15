e67e5d67ba1e3918228a184d293b778be67146c4
Ismail Sunni <imajimatika@gmail.com>
2024-07-15 13:33:38 +0700

    PB-773: Add deselect callback function.

--- src/modules/map/components/openlayers/utils/useVectorLayer.composable.js
+++ src/modules/map/components/openlayers/utils/useVectorLayer.composable.js
@@ -1,5 +1,6 @@
 import { Select } from 'ol/interaction'
 import { Vector as VectorLayer } from 'ol/layer'
+import { unByKey } from 'ol/Observable'
 import { Vector as VectorSource } from 'ol/source'
 import { onUnmounted, toValue, watchEffect } from 'vue'
 
@@ -12,7 +13,8 @@ export default function useVectorLayer(
     features,
     zIndex = -1,
     styleFunction = highlightFeatureStyle,
-    onFeatureSelectCallback = () => {} // Add onFeatureSelect parameter with a default no-op function
+    onFeatureSelectCallback = () => {}, // Add onFeatureSelect parameter with a default no-op function,
+    onFeatureDeselectCallback = () => {} // Optional: Callback for when features are deselected
 ) {
     const layer = new VectorLayer({
         id: `vector-layer-${randomIntBetween(0, 100000)}`,
@@ -26,15 +28,32 @@ export default function useVectorLayer(
     // Create and add the Select interaction to the map
     const selectInteraction = new Select({
         layers: [layer], // Only apply the interaction to this layer
-        style: null,
+        style: null, // Do not update the style of the selected features
     })
     map.addInteraction(selectInteraction)
 
-    // Listen for feature selection (click events)
+    // Listen for feature selection
     selectInteraction.on('select', function (event) {
-        event.selected.forEach((feature) => {
-            onFeatureSelectCallback(feature)
+        if (event.selected.length > 0) {
+            event.selected.forEach((feature) => {
+                onFeatureSelectCallback(feature)
+            })
+        } else if (event.deselected.length > 0) {
+            onFeatureDeselectCallback() // Call the deselect callback
+        }
+    })
+
+    // Add a click listener to the map to handle deselection
+    const mapClickListener = map.on('click', function (evt) {
+        const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+            return feature
         })
+
+        if (!feature) {
+            // If no feature is clicked, clear the selection
+            selectInteraction.getFeatures().clear()
+            onFeatureDeselectCallback() // Call the deselect callback
+        }
     })
 
     watchEffect(() => {
@@ -45,6 +64,7 @@ export default function useVectorLayer(
     // Clean up: remove the interaction when the composable is unmounted
     onUnmounted(() => {
         map.removeInteraction(selectInteraction)
+        unByKey(mapClickListener) // Remove the click listener from the map
     })
 
     return {
