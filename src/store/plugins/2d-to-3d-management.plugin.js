3e36d4a49048cd9eb08216543998878323c064e4
Brice Schaffner <brice.schaffner@swisstopo.ch>
2024-08-12 16:43:53 +0200

    PB-154: Avoid out of bound error in geolocation when switching to 3D

--- src/store/plugins/2d-to-3d-management.plugin.js
+++ src/store/plugins/2d-to-3d-management.plugin.js
@@ -1,3 +1,5 @@
+import proj4 from 'proj4'
+
 import { DEFAULT_PROJECTION } from '@/config/map.config'
 import { WEBMERCATOR } from '@/utils/coordinates/coordinateSystems'
 
@@ -15,11 +17,35 @@ export default function from2Dto3Dplugin(store) {
             if (action.type === 'set3dActive') {
                 if (DEFAULT_PROJECTION.epsg !== WEBMERCATOR.epsg) {
                     if (state.cesium.active) {
+                        // We also need to re-project the geolocation position
+                        if (state.geolocation.position) {
+                            const geolocationPosition = proj4(
+                                state.position.projection.epsg,
+                                WEBMERCATOR.epsg,
+                                state.geolocation.position
+                            )
+                            store.dispatch('setGeolocationPosition', {
+                                position: geolocationPosition,
+                                ...dispatcher,
+                            })
+                        }
                         store.dispatch('setProjection', {
                             projection: WEBMERCATOR,
                             ...dispatcher,
                         })
                     } else {
+                        // We also need to re-project the geolocation position
+                        if (state.geolocation.position) {
+                            const geolocationPosition = proj4(
+                                state.position.projection.epsg,
+                                DEFAULT_PROJECTION.epsg,
+                                state.geolocation.position
+                            )
+                            store.dispatch('setGeolocationPosition', {
+                                position: geolocationPosition,
+                                ...dispatcher,
+                            })
+                        }
                         store.dispatch('setProjection', {
                             projection: DEFAULT_PROJECTION,
                             ...dispatcher,
