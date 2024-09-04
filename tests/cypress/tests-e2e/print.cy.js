364203dc39bcf8df28a29e1f5480311fcd5f151c
Ismail Sunni <imajimatika@gmail.com>
2024-09-04 15:45:38 +0200

    PB-878: Set center to fix failed test in CI.

--- tests/cypress/tests-e2e/print.cy.js
+++ tests/cypress/tests-e2e/print.cy.js
@@ -192,6 +192,7 @@ describe('Testing print', () => {
                 {
                     layers: `KML|${getServiceKmlBaseUrl()}some-kml-file.kml`,
                     z: zoom || 15,
+                    center: '2655000,1203250',
                 },
                 true
             )
