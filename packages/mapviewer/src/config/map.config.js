ae2d5a27c05215cbb460a1b736bc9fecf5c28f5a
Pascal Barth <pak.barth@gmail.com>
2024-09-20 10:14:48 +0200

    [POC] Support on-the-fly LV95 reprojection
    copying geoblocks/ol-maplibre-layer/ locally to be able to edit it and add the on the fly capabilities (will create a PR there when done)
    
    Making our coordinate system calculate resolution without threshold (especially for LV95) so that it can then be used to calculate a mercator zoom level

--- packages/mapviewer/src/config/map.config.js
+++ packages/mapviewer/src/config/map.config.js
@@ -1,11 +1,11 @@
-import { WEBMERCATOR } from '@geoadmin/coordinates'
+import { LV95 } from '@geoadmin/coordinates'
 
 /**
  * Default projection to be used throughout the application
  *
  * @type {CoordinateSystem}
  */
-export const DEFAULT_PROJECTION = WEBMERCATOR
+export const DEFAULT_PROJECTION = LV95
 
 /**
  * Default tile size to use when requesting WMS tiles with our internal WMSs (512px)
