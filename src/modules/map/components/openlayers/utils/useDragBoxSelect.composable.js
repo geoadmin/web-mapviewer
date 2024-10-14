e24f8a65aa97d48220625126172491e891c2be67
Felix Sommer <felix.sommer@camptocamp.com>
2024-10-14 16:17:01 +0200

    PB-875: select kml gpx and geojson features with drag box

--- src/modules/map/components/openlayers/utils/useDragBoxSelect.composable.js
+++ src/modules/map/components/openlayers/utils/useDragBoxSelect.composable.js
@@ -1,8 +1,16 @@
+import { booleanIntersects } from '@turf/boolean-intersects'
+import { lineString, point, polygon } from '@turf/helpers'
 import { platformModifierKeyOnly } from 'ol/events/condition'
+import GeoJSON from 'ol/format/GeoJSON'
+import { LineString, Point, Polygon } from 'ol/geom'
 import { DragBox } from 'ol/interaction'
 import { useStore } from 'vuex'
 
+import LayerTypes from '@/api/layers/LayerTypes.enum'
 import { ClickInfo } from '@/store/modules/map.store'
+import { parseGpx } from '@/utils/gpxUtils'
+import { parseKml } from '@/utils/kmlUtils'
+import { createLayerFeature } from '@/utils/layerUtils'
 
 const dispatcher = {
     dispatcher: 'useDragBoxSelect.composable',
@@ -20,16 +28,56 @@ export function useDragBoxSelect() {
     )
     dragBoxSelect.on('boxend', () => {
         const selectExtent = dragBoxSelect.getGeometry()?.getExtent()
+        if (selectExtent?.length !== 4) return
 
-        if (selectExtent?.length === 4) {
-            store.dispatch('click', {
-                clickInfo: new ClickInfo({ coordinate: selectExtent }),
-                ...dispatcher,
+        const dragBox = polygon(dragBoxSelect.getGeometry()?.getCoordinates())
+        const visibleLayers = store.getters.visibleLayers.filter((layer) =>
+            [LayerTypes.GEOJSON, LayerTypes.GPX, LayerTypes.KML].includes(layer.type)
+        )
+        const features = visibleLayers
+            .flatMap((layer) => {
+                if (layer.type === LayerTypes.KML) {
+                    const kmlFeatures = parseKml(layer, store.state.position.projection, [])
+                    return kmlFeatures.map((feature) => ({ feature: feature, layer }))
+                }
+                if (layer.type === LayerTypes.GPX) {
+                    const gpxFeatures = parseGpx(layer.gpxData, store.state.position.projection, [])
+                    return gpxFeatures.map((feature) => ({ feature: feature, layer }))
+                }
+                if (layer.type === LayerTypes.GEOJSON) {
+                    const geojsonFormat = new GeoJSON()
+                    const olFeatures = geojsonFormat.readFeatures(layer.geoJsonData, {
+                        featureProjection: store.state.position.projection.epsg,
+                    })
+                    return olFeatures.map((feature) => ({ feature: feature, layer }))
+                }
             })
-        }
+            .filter((result) => {
+                const geometry = fromOlGeometryToTurfGeometry(result.feature.getGeometry())
+                return geometry && dragBox && booleanIntersects(dragBox, geometry)
+            })
+            .map(({ feature, layer }) => createLayerFeature(feature, layer))
+
+        store.dispatch('click', {
+            clickInfo: new ClickInfo({ coordinate: selectExtent, features: features }),
+            ...dispatcher,
+        })
     })
 
     return {
         dragBoxSelect,
     }
+}
+
+function fromOlGeometryToTurfGeometry(olGeometry) {
+    if (olGeometry instanceof Point) {
+        return point(olGeometry.getCoordinates())
+    }
+    if (olGeometry instanceof Polygon) {
+        return polygon(olGeometry.getCoordinates())
+    }
+    if (olGeometry instanceof LineString) {
+        return lineString(olGeometry.getCoordinates())
+    }
+    return null
 }
