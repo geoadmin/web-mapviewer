3494442f62f8c42e2013f1cb8195fe17f0634585
Felix Sommer <felix.sommer@camptocamp.com>
2024-10-25 13:24:26 +0200

    PB-423: exit full screen escape

--- src/modules/map/components/toolbox/FullScreenButton.vue
+++ src/modules/map/components/toolbox/FullScreenButton.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { computed } from 'vue'
+import { computed, onUnmounted } from 'vue'
+import { onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { useStore } from 'vuex'
 
 import { useTippyTooltip } from '@/utils/composables/useTippyTooltip'
 
 const dispatcher = { dispatcher: 'FullScreenButton.vue' }
 
+const i18n = useI18n()
 const store = useStore()
 
 useTippyTooltip('#fullscreenButton[data-tippy-content]', { placement: 'left' })
@@ -18,21 +21,40 @@ function toggleFullScreen() {
 }
 const height = computed(() => store.state.ui.height)
 const width = computed(() => store.state.ui.width)
-const isInWindowFullScreenMode = computed(
-    () => screen.width === width.value && screen.height === height.value
+const isInWindowFullScreenModeNotChromium = computed(
+    () => screen.width === width.value && screen.height === height.value && !window.chrome
 )
+onMounted(() => {
+    window.addEventListener('keydown', handleKeydown)
+})
+onUnmounted(() => {
+    window.removeEventListener('keydown', handleKeydown)
+})
+
+function handleKeydown(event) {
+    if (event.key === 'Escape' && isInFullScreenMode.value) {
+        toggleFullScreen()
+    }
+}
+
+const tippyContent = computed(() => {
+    if (isInFullScreenMode.value) {
+        return 'full_screen_exit'
+    }
+    return 'full_screen'
+})
 </script>
 
 <template>
-    <div v-if="isInWindowFullScreenMode" class="fullscreen-warning">
-        To exit full screen press F11.
+    <div v-if="isInWindowFullScreenModeNotChromium" class="fullscreen-warning">
+        {{ i18n.t('full_screen_window_exit') }}
     </div>
     <button
         id="fullscreenButton"
         class="toolbox-button d-print-none"
         :class="{ active: isInFullScreenMode }"
         data-cy="toolbox-fullscreen-button"
-        data-tippy-content="full_screen"
+        :data-tippy-content="tippyContent"
         @click="toggleFullScreen()"
     >
         <FontAwesomeIcon icon="expand" />
