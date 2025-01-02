aff9d4fb7729150ec45d85f701fc16732fa682fe
Ismail Sunni <imajimatika@gmail.com>
2025-01-02 16:18:53 +0700

    PB-1197: Add missing file.

---
+++ tests/cypress/tests-e2e/utils.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+export function moveTimeSlider(x) {
+    cy.get('[data-cy="time-slider-bar-cursor-grab"]').trigger('mousedown', { which: 1 })
+    cy.get('[data-cy="time-slider-bar-cursor-grab"]').trigger('mousemove', {
+        screenX: Math.abs(x),
+        screenY: 0,
+    })
+    cy.get('[data-cy="time-slider-bar-cursor-grab"]').trigger('mouseup', { force: true })
+}
