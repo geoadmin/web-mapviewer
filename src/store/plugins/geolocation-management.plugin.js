bb6173b64146f18dede37df91ff9e0dc9edbff8c
Ismail Sunni <imajimatika@gmail.com>
2025-01-09 12:39:29 +0700

    PB-1187: Address PR request.

--- src/store/plugins/geolocation-management.plugin.js
+++ src/store/plugins/geolocation-management.plugin.js
@@ -98,52 +98,41 @@ const handlePositionError = (error, store, state, options = {}) => {
                 ...dispatcher,
             })
             store.dispatch('addErrors', {
-                errors: [new ErrorMessage('geoloc_permission_denied', null)],
+                errors: [new ErrorMessage('geoloc_permission_denied')],
                 ...dispatcher,
             })
             break
         case GeolocationPositionError.TIMEOUT:
             store.dispatch('setGeolocation', { active: false, ...dispatcher })
             store.dispatch('addErrors', {
-                errors: [new ErrorMessage('geoloc_time_out', null)],
+                errors: [new ErrorMessage('geoloc_time_out')],
                 ...dispatcher,
             })
             break
         default:
-            if (
-                IS_TESTING_WITH_CYPRESS &&
-                error.code === GeolocationPositionError.POSITION_UNAVAILABLE
-            ) {
-                store.dispatch('setGeolocation', { active: false, ...dispatcher })
+            // It can happen that the position is not yet available so we retry the api call silently for the first
+            // 3 call
+            errorCount += IS_TESTING_WITH_CYPRESS ? 3 : 1
+            if (errorCount < 3) {
+                if (reactivate) {
+                    activeGeolocation(store, state, { useInitial: false })
+                }
+            } else {
                 store.dispatch('addErrors', {
-                    errors: [new ErrorMessage('geoloc_unknown', null)],
+                    errors: [new ErrorMessage('geoloc_unknown')],
                     ...dispatcher,
                 })
-            } else {
-                // It can happen that the position is not yet available so we retry the api call silently for the first
-                // 3 call
-                errorCount += 1
-                if (errorCount < 3) {
-                    if (reactivate) {
-                        activeGeolocation(store, state, { useInitial: false })
-                    }
-                } else {
-                    store.dispatch('addErrors', {
-                        errors: [new ErrorMessage('geoloc_unknown', null)],
-                        ...dispatcher,
-                    })
-                    if (reactivate) {
-                        // If after 3 retries we failed to re-activate, set the geolocation to false
-                        // so that the user can manually retry the geolocation later on. This can
-                        // mean that the device don't support geolocation so it doesn't make sense
-                        // to retry for ever.
-                        // In the case where we are in the watcher, this means that we had at least
-                        // one successful location and that geolocation is supported by the device.
-                        // So we let the watcher continue has he might recover itself later on, if
-                        // not the error will kept showing and the user will have to manually stop
-                        // geolocation.
-                        store.dispatch('setGeolocation', { active: false, ...dispatcher })
-                    }
+                if (reactivate) {
+                    // If after 3 retries we failed to re-activate, set the geolocation to false
+                    // so that the user can manually retry the geolocation later on. This can
+                    // mean that the device don't support geolocation so it doesn't make sense
+                    // to retry for ever.
+                    // In the case where we are in the watcher, this means that we had at least
+                    // one successful location and that geolocation is supported by the device.
+                    // So we let the watcher continue has he might recover itself later on, if
+                    // not the error will kept showing and the user will have to manually stop
+                    // geolocation.
+                    store.dispatch('setGeolocation', { active: false, ...dispatcher })
                 }
             }
     }
