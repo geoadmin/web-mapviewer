15488db098148bb440eb1d905c65c5c222cd5b49
Sami Nouidri <sami.nouidri@swisstopo.ch>
2025-02-07 14:48:45 +0100

    PB-1318 : added kml deletion modification with new mapviewer structure
    Please refer to PR #1224 for original git history and review

--- packages/mapviewer/src/modules/drawing/components/DrawingToolbox.vue
+++ packages/mapviewer/src/modules/drawing/components/DrawingToolbox.vue
@@ -21,7 +21,7 @@ import debounce from '@/utils/debounce'
 const dispatcher = { dispatcher: 'DrawingToolbox.vue' }
 
 const drawingLayer = inject('drawingLayer')
-const { saveState, debounceSaveDrawing } = useSaveKmlOnChange()
+const { saveState, deleteDrawing, debounceSaveDrawing } = useSaveKmlOnChange()
 const { t } = useI18n()
 const store = useStore()
 
@@ -100,7 +100,7 @@ function onCloseClearConfirmation(confirmed) {
         store.dispatch('setIsDrawingModified', { value: false, ...dispatcher })
         store.dispatch('setIsDrawingEditShared', { value: false, ...dispatcher })
         drawingLayer.getSource().clear()
-        debounceSaveDrawing()
+        deleteDrawing()
         store.dispatch('setDrawingMode', { mode: null, ...dispatcher })
         store.dispatch('removeLayer', {
             layerId: activeKmlLayer.value.id,
@@ -185,10 +185,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
                     ref="drawingNameContainer"
                     class="d-flex justify-content-center align-items-center gap-2 mt-3 mx-4"
                 >
-                    <label
-                        for="drawing-name"
-                        class="text-nowrap"
-                    >
+                    <label for="drawing-name" class="text-nowrap">
                         {{ t('file_name') }}
                     </label>
                     <input
@@ -199,7 +196,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
                         data-cy="drawing-toolbox-file-name-input"
                         :placeholder="`${t('draw_layer_label')}`"
                         :disabled="!activeKmlLayer"
-                    >
+                    />
                 </div>
 
                 <div class="card-body position-relative container">
@@ -257,10 +254,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
                         <div class="col d-grid">
                             <DrawingExporter :is-drawing-empty="isDrawingEmpty" />
                         </div>
-                        <div
-                            v-if="online"
-                            class="col d-grid"
-                        >
+                        <div v-if="online" class="col d-grid">
                             <button
                                 type="button"
                                 class="btn btn-light"
@@ -286,10 +280,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
                             </button>
                         </div>
                     </div>
-                    <div
-                        v-if="isDesktopMode && online"
-                        class="row mt-2"
-                    >
+                    <div v-if="isDesktopMode && online" class="row mt-2">
                         <div
                             class="col text-center text-muted"
                             data-cy="drawing-toolbox-disclaimer"
@@ -301,10 +292,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
                     </div>
                 </div>
             </div>
-            <div
-                v-if="isDesktopMode"
-                class="text-center"
-            >
+            <div v-if="isDesktopMode" class="text-center">
                 <button
                     class="button-open-close-draw-menu btn btn-dark m-auto ps-4 pe-4 rounded-0 rounded-bottom"
                     data-cy="menu-button"
@@ -337,10 +325,7 @@ const debounceSaveDrawingName = debounce(async (newName) => {
             :title="t('warning')"
             @close="onCloseWarningModal()"
         >
-            <ShareWarningPopup
-                :kml-layer="activeKmlLayer"
-                @accept="onAcceptWarningModal()"
-            />
+            <ShareWarningPopup :kml-layer="activeKmlLayer" @accept="onAcceptWarningModal()" />
         </ModalWithBackdrop>
     </teleport>
 </template>
