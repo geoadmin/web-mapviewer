5e515ffa0da63cdf5fdab76ecbc9d9cde254cd46
Ismail Sunni <imajimatika@gmail.com>
2025-02-13 14:54:38 +0700

    PB-577: Put BLOCKED_EXTENSIONS and WHITELISTED_HOSTNAMES to security.config.js

--- packages/mapviewer/src/config/staging.config.js
+++ packages/mapviewer/src/config/staging.config.js
@@ -64,17 +64,3 @@ export const REPORT_PROBLEM_HOSTNAMES = [
     'sys-map.prod.bgdi.ch',
     'map.geo.admin.ch',
 ]
-
-/**
- * Don't show third party disclaimer for iframe with one of these hosts as src
- *
- * @type {String[]}
- */
-export const WHITELISTED_HOSTNAMES = ['test.map.geo.admin.ch', 'map.geo.admin.ch']
-
-/**
- * List of blocked file extensions
- *
- * @type {String[]}
- */
-export const BLOCKED_EXTENSIONS = ['exe', 'bat', 'cmd', 'sh', 'msi', 'scr', 'vbs', 'dll']
