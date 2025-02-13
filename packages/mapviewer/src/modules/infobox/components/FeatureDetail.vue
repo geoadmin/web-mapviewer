be3c7555082f5b305375182818ac1cd358fca54e
Ismail Sunni <imajimatika@gmail.com>
2025-02-13 13:59:15 +0700

    PB-577: Fix lint.

--- packages/mapviewer/src/modules/infobox/components/FeatureDetail.vue
+++ packages/mapviewer/src/modules/infobox/components/FeatureDetail.vue
@@ -62,6 +62,7 @@ function sanitizeHtml(htmlText, withIframe = false) {
                     return
                 }
             } catch (error) {
+                log.error(error)
                 node.outerHTML = blockedExternalContentString
                 return
             }
@@ -75,6 +76,7 @@ function sanitizeHtml(htmlText, withIframe = false) {
                     return
                 }
             } catch (error) {
+                log.error(error)
                 node.outerHTML = blockedExternalContentString
                 return
             }
@@ -109,10 +111,19 @@ function getIframeHosts(value) {
 
 <template>
     <!-- eslint-disable vue/no-v-html-->
-    <div v-if="hasFeatureStringData && popupDataCanBeTrusted" v-html="feature.data" />
-    <div v-else-if="hasFeatureStringData" v-html="sanitizeHtml(feature.data)" />
+    <div
+        v-if="hasFeatureStringData && popupDataCanBeTrusted"
+        v-html="feature.data"
+    />
+    <div
+        v-else-if="hasFeatureStringData"
+        v-html="sanitizeHtml(feature.data)"
+    />
     <!-- eslint-enable vue/no-v-html-->
-    <div v-else class="htmlpopup-container">
+    <div
+        v-else
+        class="htmlpopup-container"
+    >
         <div class="htmlpopup-content">
             <div
                 v-for="[key, value, externalIframeHosts] in sanitizedFeatureDataEntries"
@@ -125,11 +136,17 @@ function getIframeHosts(value) {
                     :external-iframe-hosts="externalIframeHosts"
                     :title="key"
                 />
-                <div v-else class="fw-bold">
+                <div
+                    v-else
+                    class="fw-bold"
+                >
                     {{ t(key) }}
                 </div>
                 <!-- eslint-disable vue/no-v-html-->
-                <div data-cy="feature-detail-description-content" v-html="value" />
+                <div
+                    data-cy="feature-detail-description-content"
+                    v-html="value"
+                />
                 <!-- eslint-enable vue/no-v-html-->
             </div>
             <div v-if="sanitizedFeatureDataEntries.length === 0">
@@ -147,7 +164,10 @@ function getIframeHosts(value) {
                 :value="feature.geometry.coordinates.slice(0, 2)"
                 :coordinate-format="coordinateFormat"
             >
-                <FontAwesomeIcon class="small align-text-top" icon="fas fa-map-marker-alt" />
+                <FontAwesomeIcon
+                    class="small align-text-top"
+                    icon="fas fa-map-marker-alt"
+                />
             </CoordinateCopySlot>
         </div>
     </div>
