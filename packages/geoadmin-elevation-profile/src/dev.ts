dff0d8b196d38c500b521f4cc803f2cbca09b425
Pascal Barth <pak.barth@gmail.com>
2025-02-18 07:12:25 +0100

    PB-1384 : profile chart as stand alone module

---
+++ packages/geoadmin-elevation-profile/src/dev.ts
@@ -0,0 +1,17 @@
+import '@/index.ts'
+
+import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
+
+import DevApp from '@/DevApp.vue'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    messages: {},
+    missingWarn: false,
+})
+
+const app = createApp(DevApp)
+app.use(i18n)
+app.mount('#app')
