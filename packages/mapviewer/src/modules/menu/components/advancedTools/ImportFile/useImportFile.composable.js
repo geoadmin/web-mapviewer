95dec06c82a265c784b9239531cdbd7a1130f16e
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-03-14 10:43:29 +0100

    PB-1151: Add warning messages to layers
    - Added warning messages to layers
    - When an external layer is partially out of bound, it will store a warning message in the layer about it.
    - translations

--- packages/mapviewer/src/modules/menu/components/advancedTools/ImportFile/useImportFile.composable.js
+++ packages/mapviewer/src/modules/menu/components/advancedTools/ImportFile/useImportFile.composable.js
@@ -4,6 +4,7 @@ import { useStore } from 'vuex'
 
 import { parseLayerFromFile } from '@/modules/menu/components/advancedTools/ImportFile/parser'
 import generateErrorMessageFromErrorType from '@/modules/menu/components/advancedTools/ImportFile/parser/errors/generateErrorMessageFromErrorType.utils'
+import WarningMessage from '@/utils/WarningMessage.class'
 
 const dispatcher = {
     dispatcher: 'useImportFile.composable',
@@ -18,9 +19,11 @@ export default function useImportFile() {
      * @param {File | String} source
      * @param {Boolean} [sendErrorToStore=true] If true, will dispatch any error to the store, if
      *   false will throw any error at you. Default is `true`
+     * @param {Boolean} [sendWarningToStore=true] If true, will dispatch any warning to the store,
+     *   if false, it will ignore it. Default is `true`
      * @returns {Promise<void>}
      */
-    async function handleFileSource(source, sendErrorToStore = true) {
+    async function handleFileSource(source, sendErrorToStore = true, sendWarningToStore = true) {
         if (!source) {
             return
         }
@@ -30,13 +33,37 @@ export default function useImportFile() {
         })
         try {
             const layer = await parseLayerFromFile(source, projection.value)
+
             // checking that the same layer is not already present before adding it
             if (store.getters.getActiveLayersById(layer.id).length === 0) {
+                const extent =
+                    layer.extent.length === 4
+                        ? layer.extent
+                        : [...layer.extent[0], ...layer.extent[1]]
+                const isLayerFullyInBound =
+                    projection.value.bounds.isInBounds(extent[0], extent[1]) &&
+                    projection.value.bounds.isInBounds(extent[2], extent[3])
+                if (!isLayerFullyInBound) {
+                    layer.hasWarning = true
+                    layer.addWarningMessage(
+                        new WarningMessage('file_imported_partially_out_of_bounds', {
+                            filename: layer.name ?? layer.id,
+                        })
+                    )
+                }
+
                 await store.dispatch('addLayer', {
                     layer,
                     zoomToLayerExtent: true,
                     ...dispatcher,
                 })
+
+                if (sendWarningToStore && layer.hasWarning) {
+                    store.dispatch('addWarnings', {
+                        warnings: Array.from(layer.warningMessages),
+                        ...dispatcher,
+                    })
+                }
             }
         } catch (error) {
             if (!sendErrorToStore) {
