5a0e36c1321ec48748a81f16897f2d7f39864d8f
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-04-04 11:25:57 +0200

    PB-1383 Install vite-tsconfig-paths for the unit tests
    As the unit tests are mixed js and ts, this is apparently what's needed
    in order for vitest to work in vscode

--- packages/mapviewer/src/router/storeSync/__tests__/layersParamParser.spec.js
+++ packages/mapviewer/src/router/storeSync/__tests__/layersParamParser.spec.js
@@ -14,6 +14,7 @@ import LayerTimeConfigEntry from '@/api/layers/LayerTimeConfigEntry.class.js'
 import LayerTypes from '@/api/layers/LayerTypes.enum'
 import { getServiceKmlBaseUrl } from '@/config/baseUrl.config'
 import { parseLayersParam, transformLayerIntoUrlString } from '@/router/storeSync/layersParamParser'
+import { makeKmlLayer } from '@/utils/kmlUtils'
 
 describe('Testing layersParamParser', () => {
     const checkParsedLayer = (
