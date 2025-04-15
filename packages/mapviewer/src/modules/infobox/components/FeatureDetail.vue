1a014974e81de2f89fefb9eecc6aadb22cdcb351
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-04-15 09:07:58 +0200

    PB-1561: revert CSS changes to generic feature pop up windows
    Issue : we adapted the CSS for the generic pop up which holds the 3d features info for now
    , but the styling doesn't please our users who are using the drawings.
    Fix : We revert the changes and the 3d pop up will be done when we can get it
    from the backend :)

--- packages/mapviewer/src/modules/infobox/components/FeatureDetail.vue
+++ packages/mapviewer/src/modules/infobox/components/FeatureDetail.vue
@@ -128,16 +128,18 @@ function getIframeHosts(value) {
                     :external-iframe-hosts="externalIframeHosts"
                     :title="key"
                 />
-                <div class="d-flex flex-row justify-content-between">
-                    <div class="fw-bold">{{ t(key) }}</div>
-                    <!-- eslint-disable vue/no-v-html-->
-                    <div
-                        data-cy="feature-detail-description-content"
-                        class="text-end pr-3"
-                        v-html="t(value)"
-                    ></div>
-                    <!-- eslint-enable vue/no-v-html-->
+                <div
+                    v-else
+                    class="fw-bold"
+                >
+                    {{ t(key) }}
                 </div>
+                <!-- eslint-disable vue/no-v-html-->
+                <div
+                    data-cy="feature-detail-description-content"
+                    v-html="t(value)"
+                ></div>
+                <!-- eslint-enable vue/no-v-html-->
             </div>
             <div v-if="sanitizedFeatureDataEntries.length === 0">
                 {{ t('no_more_information') }}
