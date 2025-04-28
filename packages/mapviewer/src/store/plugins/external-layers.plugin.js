47e3b4e2d3586576ac105ae25911847714b72002
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-04-28 14:03:34 +0200

    PB-1253: stop external layers to never be ready
    Issue : Whenever we loaded external layers with parameters in the base URL, we sometimes had issues where the base Url of the get Capabilities and the base URL of the layer were not the same, thus blocking the equality check that allowed the layer to update. This caused the layer to never show that it was ready, even when it was.
    Fix : When we update the external layer through its capabilities, if there was a baseUrl before, we set the baseUrl of the `updated` layer to the original one, ensuring the layer will be updated.

--- packages/mapviewer/src/store/plugins/external-layers.plugin.js
+++ packages/mapviewer/src/store/plugins/external-layers.plugin.js
@@ -101,6 +101,13 @@ async function updateExternalLayer(store, capabilities, layer, projection) {
             layer.customAttributes,
             false /* throw Error in case of  error */
         )
+        if (layer.baseUrl) {
+            // in some situations, the baseUrl of the capabilities is not the
+            // same as the one being given by the user. We ensure we keep the
+            // same baseUrl so we can pass the baseUrl check in the `setUpdateLayer`
+            // later
+            updated.baseUrl = layer.baseUrl
+        }
         updated.isLoading = false
         return updated
     } catch (error) {
