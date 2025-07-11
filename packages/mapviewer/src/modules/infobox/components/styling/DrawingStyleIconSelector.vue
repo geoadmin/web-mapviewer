00964396e343e8ac3730016e3b182e41d9875cf3
Pascal Barth <pak.barth@gmail.com>
2025-07-11 14:31:14 +0200

    PB-1750: fix print scales going out of screen (overflow)
    refactoring the DropdownButton component to use FloatingUI, and ensuring the size of the dropdown "menu" (aka the floating thing) can't grow outside the document's height, and switch to a scroll/overflow if needed

--- packages/mapviewer/src/modules/infobox/components/styling/DrawingStyleIconSelector.vue
+++ packages/mapviewer/src/modules/infobox/components/styling/DrawingStyleIconSelector.vue
@@ -7,7 +7,6 @@ import DrawingStyleColorSelector from '@/modules/infobox/components/styling/Draw
 import DrawingStyleIcon from '@/modules/infobox/components/styling/DrawingStyleIcon.vue'
 import DrawingStyleSizeSelector from '@/modules/infobox/components/styling/DrawingStyleSizeSelector.vue'
 import DropdownButton from '@/utils/components/DropdownButton.vue'
-import DropdownButtonItem from '@/utils/components/DropdownButtonItem.vue'
 
 const { feature, iconSets } = defineProps({
     feature: {
@@ -105,16 +104,11 @@ function onCurrentIconChange(icon) {
                 </label>
                 <DropdownButton
                     :title="currentIconSetName"
+                    :items="iconSetDropdownItems"
+                    :current-value="currentIconSet"
                     data-cy="drawing-style-icon-set-button"
-                >
-                    <DropdownButtonItem
-                        v-for="item in iconSetDropdownItems"
-                        :key="item.id"
-                        v-bind="item"
-                        :current-value="currentIconSet"
-                        @select-item="changeDisplayedIconSet"
-                    />
-                </DropdownButton>
+                    @select-item="changeDisplayedIconSet"
+                />
             </div>
         </div>
         <DrawingStyleColorSelector
