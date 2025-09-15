9be930bc73e565043c22379ff7edea2fc308bf3f
Felix Sommer <felix.sommer@camptocamp.com>
2025-09-15 14:29:30 +0200

    PB-1383: migrate test footer typescript

--- packages/viewer/tests/cypress/tests-e2e/footer.cy.ts
+++ packages/viewer/tests/cypress/tests-e2e/footer.cy.ts
@@ -4,13 +4,63 @@ import { WEBMERCATOR } from '@swissgeo/coordinates'
 
 import { UIModes } from '@/store/modules/ui.store'
 
+interface LayerConfig {
+    // common properties
+    opacity: number
+    type: string
+
+    background: boolean
+    searchable: boolean
+    tooltip: boolean
+    timeEnabled: boolean
+    highlightable: boolean
+    chargeable: boolean
+    hasLegend: boolean
+
+    attribution: string
+    attributionUrl?: string
+
+    config3d?: string
+
+    topics: string
+
+    label: string
+    serverLayerName: string
+
+    timestamps?: string[]
+    timeBehaviour?: string
+
+    // WMS specific properties
+    singleTile?: boolean
+    wmsUrl?: string
+    wmsLayers?: string
+    gutter?: number
+
+    // Common to WMTS and WMS
+    format?: 'jpeg' | 'png'
+
+    queryableAttributes?: string[]
+    resolutions?: number[]
+
+    // GeoJSON specific properties
+    geojsonUrl?: string
+    styleUrl?: string
+    updateDelay?: number
+
+    // Aggregate specific properties
+    subLayersIds?: string[]
+    minResolution?: number
+    maxResolution?: number
+}
+
+
 describe('Testing the footer content / tools', () => {
     it('shows/hide the scale line depending on the map resolution, while in Mercator', () => {
         cy.viewport(1920, 1080)
-        cy.goToMapView({queryParams:{sr: WEBMERCATOR.epsgNumber}})
+        cy.goToMapView({ queryParams: { sr: WEBMERCATOR.epsgNumber } })
 
         // Scale line not visible on standard startup, as the standard zoom level is 7,
-        // which mean a higher resolution as the acceptable threshold for scale line to be visible.
+        // which means a higher resolution than the acceptable threshold for the scale line to be visible
         cy.get('[data-cy="scaleline"]').should('not.be.visible')
 
         // triple zoom, the scale line should appear (zoom level 10)
@@ -25,18 +75,20 @@ describe('Testing the footer content / tools', () => {
         cy.get('[data-cy="zoom-out"]').click()
         cy.get('[data-cy="scaleline"]').should('not.be.visible')
     })
+
     it('has a functional background wheel', () => {
-        function testBackgroundWheel() {
+        function testBackgroundWheel(desktop: boolean = false): void {
+            const wheelButton = desktop ? 'background-selector-open-wheel-button-squared' : 'background-selector-open-wheel-button'
             // first, checking that the current bgLayer is the void layer
             cy.readStoreValue('getters.currentBackgroundLayer').should('be.null')
 
             // opening the background wheel
-            cy.get('[data-cy="background-selector-open-wheel-button"]').click()
+            cy.get(`[data-cy="${wheelButton}"]`).click()
 
             // checking that all layers flagged as backgrounds are represented in the wheel
-            cy.fixture('layers.fixture').then((layers) => {
+            cy.fixture('layers.fixture').then((layers: Record<string, LayerConfig>) => {
                 Object.values(layers)
-                    .filter((layer) => layer.background && layer.idIn3d)
+                    .filter((layer) => layer.background)
                     .forEach((bgLayer) => {
                         cy.get(`[data-cy="background-selector-${bgLayer.serverLayerName}"]`).click()
                         // checking that clicking on the wheel buttons changes the bgLayer of the app accordingly
@@ -45,7 +97,7 @@ describe('Testing the footer content / tools', () => {
                                 state.layers.currentBackgroundLayerId === bgLayer.serverLayerName
                         )
                         // reopening the BG wheel
-                        cy.get('[data-cy="background-selector-open-wheel-button"]').click()
+                        cy.get(`[data-cy="${wheelButton}"]`).click()
                     })
             })
 
@@ -54,14 +106,13 @@ describe('Testing the footer content / tools', () => {
             cy.readStoreValue('getters.currentBackgroundLayer').should('be.null')
         }
 
-        cy.goToMapView({queryParams:{bgLayer: 'void'}})
+        cy.goToMapView({ queryParams: { bgLayer: 'void' } })
         // checking the rounded background wheel (mobile/tablet only)
         cy.viewport('iphone-se2')
         testBackgroundWheel()
-
         // checking that the squared background wheel (desktop) has the same functionalities
         cy.viewport('macbook-11')
         cy.waitUntilState((state) => state.ui.mode === UIModes.DESKTOP)
-        testBackgroundWheel()
+        testBackgroundWheel(true)
     })
 })
