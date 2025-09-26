4286cc89880661a15928579b0f9f591a148e16c4
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-09-26 18:03:34 +0200

    screen size management plugin

---
+++ packages/viewer/src/store/plugins/screen-size-management.plugin.ts
@@ -0,0 +1,29 @@
+import { BREAKPOINT_PHONE_HEIGHT, BREAKPOINT_PHONE_WIDTH } from '@/config/responsive.config'
+import useUIStore, { UIModes } from '@/store/modules/ui.store'
+import type { PiniaPlugin } from 'pinia'
+
+const dispatcher = { name: 'screen-size-management.plugin' }
+
+/** @param store */
+const screenSizeManagementPlugin: PiniaPlugin = () => {
+    const uiStore = useUIStore()
+
+    uiStore.$onAction(({ name, store }) => {
+        if (name === 'setSize') {
+            // listening to screen size change to decide if we should switch UI mode too
+            let wantedUiMode
+
+            if (store.width < BREAKPOINT_PHONE_WIDTH || store.height < BREAKPOINT_PHONE_HEIGHT) {
+                wantedUiMode = UIModes.PHONE
+            } else {
+                // so the UI mode DESKTOP also includes the tablet mode.
+                wantedUiMode = UIModes.DESKTOP
+            }
+            if (wantedUiMode !== store.mode) {
+                store.setUiMode(wantedUiMode, dispatcher)
+            }
+        }
+    })
+}
+
+export default screenSizeManagementPlugin
