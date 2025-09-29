10ddd1efb6f92037e80538834d6629e8d8e815a8
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-09-29 17:08:23 +0200

    fix print store

--- packages/viewer/src/store/modules/print.store.ts
+++ packages/viewer/src/store/modules/print.store.ts
@@ -44,9 +44,17 @@ const usePrintStore = defineStore('print', {
                 (attribute) => attribute.name === 'map'
             )
 
+            const params = mapAttributes?.clientParams
+
+            if (!params) {
+                return { width: 0, height: 0 }
+            }
+
+            type size = { default: number }
+
             return {
-                width: mapAttributes?.clientParams?.width?.default ?? 0,
-                height: mapAttributes?.clientParams?.height?.default ?? 0,
+                width: (params.width as size).default ?? 0,
+                height: (params.height as size).default ?? 0,
             }
         },
 
