02a99db4e62e7cdfc09fe4fc560d89c4380bfa53
Ismail Sunni <imajimatika@gmail.com>
2025-10-10 13:24:47 +0700

    PB-1383: Migrate some vue utils component to TS.

--- packages/viewer/src/modules/map/components/openlayers/OpenLayersMouseTracker.vue
+++ packages/viewer/src/modules/map/components/openlayers/OpenLayersMouseTracker.vue
@@ -5,7 +5,7 @@ import { computed, inject, nextTick, onMounted, onUnmounted, ref, useTemplateRef
 import { useStore } from 'vuex'
 
 import getHumanReadableCoordinate from '@/modules/map/components/common/mouseTrackerUtils'
-import allFormats, { LV95Format } from '@/utils/coordinates/coordinateFormat.js'
+import allFormats, { LV95Format } from '@/utils/coordinates/coordinateFormat'
 
 const dispatcher = { dispatcher: 'OpenLayersMouseTracker.vue' }
 
