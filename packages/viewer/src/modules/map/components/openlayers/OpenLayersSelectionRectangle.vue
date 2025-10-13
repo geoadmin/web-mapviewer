f567afbb20641d6d2355d55d841d64eb5af55217
Ismail Sunni <imajimatika@gmail.com>
2025-10-13 18:57:00 +0700

    PB-1383: Migrate OL maps component part 3.

--- packages/viewer/src/modules/map/components/openlayers/OpenLayersSelectionRectangle.vue
+++ packages/viewer/src/modules/map/components/openlayers/OpenLayersSelectionRectangle.vue
@@ -1,24 +1,30 @@
-<script setup lang="js">
+<script setup lang="ts">
+import type { Map } from 'ol'
+
 import Feature from 'ol/Feature'
 import { fromExtent } from 'ol/geom/Polygon'
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
 import { Stroke, Style } from 'ol/style'
 import { computed, inject, watch } from 'vue'
-import { useStore } from 'vuex'
 
 import useAddLayerToMap from '@/modules/map/components/openlayers/utils/useAddLayerToMap.composable'
+import useMapStore from '@/store/modules/map.store'
+import usePositionStore from '@/store/modules/position.store'
 
-const { zIndex } = defineProps({
-    zIndex: {
-        type: Number,
-        default: -1,
-    },
+interface Props {
+    zIndex?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    zIndex: -1,
 })
 
-const store = useStore()
-const currentProjection = computed(() => store.state.position.projection)
-const selectionExtent = computed(() => store.state.map.rectangleSelectionExtent || [])
+const positionStore = usePositionStore()
+const mapStore = useMapStore()
+
+const currentProjection = computed(() => positionStore.projection)
+const selectionExtent = computed(() => mapStore.rectangleSelectionExtent ?? [])
 
 const layer = new VectorLayer({
     source: createVectorSourceForProjection(),
@@ -30,20 +36,17 @@ const layer = new VectorLayer({
     }),
 })
 
-const olMap = inject('olMap', null)
-useAddLayerToMap(layer, olMap, () => zIndex)
+const olMap = inject<Map>('olMap')!
+useAddLayerToMap(layer, olMap, props.zIndex)
 
 watch(currentProjection, () => layer.setSource(createVectorSourceForProjection()))
 watch(selectionExtent, () => layer.setSource(createVectorSourceForProjection()))
 
-function createVectorSourceForProjection() {
-    if (!selectionExtent.value) {
-        return new VectorSource({
-            projection: currentProjection.value.epsg,
-        })
+function createVectorSourceForProjection(): VectorSource {
+    if (!selectionExtent.value || selectionExtent.value.length === 0) {
+        return new VectorSource()
     } else {
         return new VectorSource({
-            projection: currentProjection.value.epsg,
             features: [
                 new Feature({
                     geometry: fromExtent(selectionExtent.value),
