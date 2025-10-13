da3afd47dde5b12f2becc6303162b1fd445dca7b
Pascal Barth <pak.barth@gmail.com>
2025-10-13 17:19:58 +0200

    PB-1383: move OpenLayersWMSLayer to TS

--- packages/viewer/src/modules/map/components/openlayers/OpenLayersWMSLayer.vue
+++ packages/viewer/src/modules/map/components/openlayers/OpenLayersWMSLayer.vue
@@ -1,50 +1,57 @@
 <script setup lang="ts">
 /** Renders a WMS layer on the map */
-
+import type { ExternalWMSLayer, GeoAdminWMSLayer, LayerCustomAttributes } from '@swissgeo/layers'
+import { ALL_YEARS_TIMESTAMP } from '@swissgeo/layers'
+import { timeConfigUtils } from '@swissgeo/layers/utils'
 import type { Map } from 'ol'
-import type { ExternalWMSLayer, GeoAdminWMSLayer } from '@swissgeo/layers'
-
 import { extentUtils, LV95 } from '@swissgeo/coordinates'
+import log from '@swissgeo/log'
 import { cloneDeep } from 'lodash'
 import { Image as ImageLayer, Tile as TileLayer } from 'ol/layer'
 import { ImageWMS, TileWMS } from 'ol/source'
 import TileGrid from 'ol/tilegrid/TileGrid'
-import { computed, inject, watch, watchEffect } from 'vue'
+import { computed, inject, toRef, watch, watchEffect } from 'vue'
 
-import { ALL_YEARS_TIMESTAMP } from '@swissgeo/layers'
 import { getBaseUrlOverride } from '@/config/baseUrl.config'
 import { WMS_TILE_SIZE } from '@/config/map.config'
 import useAddLayerToMap from '@/modules/map/components/openlayers/utils/useAddLayerToMap.composable'
-import { useI18nStore } from '@/store/modules/i18n.store'
 import usePositionStore from '@/store/modules/position.store'
-import { getTimestampFromConfig } from '@swissgeo/layers/utils'
+import { useI18nStore } from '@/store/modules/i18n.store'
 
-interface Props {
+const {
+    wmsLayerConfig,
+    parentLayerOpacity,
+    zIndex = -1,
+} = defineProps<{
     wmsLayerConfig: GeoAdminWMSLayer | ExternalWMSLayer
     parentLayerOpacity?: number
     zIndex?: number
-}
+}>()
 
-const props = withDefaults(defineProps<Props>(), {
-    parentLayerOpacity: undefined,
-    zIndex: -1,
-})
-
-// mapping relevant store values
 const positionStore = usePositionStore()
 const i18nStore = useI18nStore()
-const projection = computed(() => positionStore.projection)
-const currentLang = computed(() => i18nStore.lang)
 
 // extracting useful info from what we've linked so far
-const layerId = computed(() => ('technicalName' in props.wmsLayerConfig ? props.wmsLayerConfig.technicalName : props.wmsLayerConfig.id))
-const wmsVersion = computed(() => props.wmsLayerConfig.wmsVersion || '1.3.0')
-const format = computed(() => props.wmsLayerConfig.format || 'png')
-const gutter = computed(() => ('gutter' in props.wmsLayerConfig ? props.wmsLayerConfig.gutter : -1) || -1)
-const opacity = computed(() => props.parentLayerOpacity ?? props.wmsLayerConfig.opacity)
-const url = computed(() => getBaseUrlOverride('wms') ?? props.wmsLayerConfig.baseUrl)
-const timestamp = computed(() => getTimestampFromConfig(props.wmsLayerConfig))
-const urlParams = computed(() => cloneDeep(props.wmsLayerConfig.customAttributes) ?? undefined)
+const layerId = computed(() => {
+    if (!wmsLayerConfig.isExternal && (wmsLayerConfig as GeoAdminWMSLayer).technicalName) {
+        return (wmsLayerConfig as GeoAdminWMSLayer).technicalName
+    }
+    return wmsLayerConfig.id
+})
+const wmsVersion = computed(() => wmsLayerConfig.wmsVersion ?? '1.3.0')
+const format = computed(() => wmsLayerConfig.format ?? 'png')
+const gutter = computed(() => {
+    if (!wmsLayerConfig.isExternal && (wmsLayerConfig as GeoAdminWMSLayer).gutter) {
+        return (wmsLayerConfig as GeoAdminWMSLayer).gutter
+    }
+    return -1
+})
+const opacity = computed(() => parentLayerOpacity ?? wmsLayerConfig.opacity)
+const url = computed(() => getBaseUrlOverride('wms') ?? wmsLayerConfig.baseUrl)
+const timestamp = computed(() => timeConfigUtils.getTimestampFromConfig(wmsLayerConfig))
+const urlParams = computed<LayerCustomAttributes | undefined>(() =>
+    cloneDeep(wmsLayerConfig.customAttributes)
+)
 
 /**
  * Definition of all relevant URL param for our WMS backends. This is because both
@@ -62,9 +69,9 @@ const wmsUrlParams = computed(() => {
         TRANSPARENT: format.value === 'png',
         LAYERS: layerId.value,
         FORMAT: `image/${format.value}`,
-        LANG: currentLang.value,
+        LANG: i18nStore.lang,
         VERSION: wmsVersion.value,
-        CRS: projection.value.epsg,
+        CRS: positionStore.projection.epsg,
         TIME: timestamp.value,
     }
     if (timestamp.value === ALL_YEARS_TIMESTAMP) {
@@ -72,7 +79,7 @@ const wmsUrlParams = computed(() => {
         // to send a request without TIME param, otherwise openlayer takes the previous TIME param.
         params.TIME = undefined
     }
-    if (urlParams.value !== undefined) {
+    if (urlParams.value) {
         params = { ...params, ...urlParams.value }
     }
     return params
@@ -80,30 +87,34 @@ const wmsUrlParams = computed(() => {
 
 let layer: TileLayer<TileWMS> | ImageLayer<ImageWMS>
 if (gutter.value !== -1) {
-    layer = new TileLayer({
+    layer = new TileLayer<TileWMS>({
         properties: {
             id: layerId.value,
-            uuid: props.wmsLayerConfig.uuid,
+            uuid: wmsLayerConfig.uuid,
         },
         opacity: opacity.value,
-        source: createSourceForProjection() as TileWMS,
+        source: createTileWMSSource(),
     })
 } else {
-    layer = new ImageLayer({
+    layer = new ImageLayer<ImageWMS>({
         properties: {
             id: layerId.value,
-            uuid: props.wmsLayerConfig.uuid,
+            uuid: wmsLayerConfig.uuid,
         },
         opacity: opacity.value,
-        source: createSourceForProjection() as ImageWMS,
+        source: createImageWMSSource(),
     })
 }
 
 setExtent()
 
 // grabbing the map from the main OpenLayersMap component and use the composable that adds this layer to the map
-const olMap = inject<Map>('olMap')!
-useAddLayerToMap(layer, olMap, props.zIndex)
+const olMap = inject<Map>('olMap')
+if (!olMap) {
+    log.error('OpenLayersMap component not found')
+    throw new Error('OpenLayersMap component not found')
+}
+useAddLayerToMap(layer, olMap, toRef(zIndex))
 
 // reacting to changes accordingly
 watch(url, (newUrl) => {
@@ -113,15 +124,17 @@ watch(url, (newUrl) => {
     }
 })
 watch(opacity, (newOpacity) => layer.setOpacity(newOpacity))
-watch(projection, () => {
-    const newSource = createSourceForProjection()
-    if (layer instanceof TileLayer) {
-        layer.setSource(newSource as TileWMS)
-    } else if (layer instanceof ImageLayer) {
-        layer.setSource(newSource as ImageWMS)
+watch(
+    () => positionStore.projection,
+    () => {
+        if (gutter.value !== -1) {
+            ;(layer as TileLayer<TileWMS>).setSource(createTileWMSSource())
+        } else {
+            ;(layer as ImageLayer<ImageWMS>).setSource(createImageWMSSource())
+        }
+        setExtent()
     }
-    setExtent()
-})
+)
 
 watch(wmsUrlParams, () => {
     const source = layer.getSource()
@@ -134,51 +147,46 @@ watchEffect(() => {
     setExtent()
 })
 
-function createSourceForProjection(): TileWMS | ImageWMS {
-    let source: TileWMS | ImageWMS
-    if (gutter.value !== -1) {
-        const tileGrid = !projection.value.usesMercatorPyramid && projection.value.bounds
+function createTileWMSSource(): TileWMS {
+    return new TileWMS({
+        projection: positionStore.projection.epsg,
+        url: url.value,
+        gutter: gutter.value,
+        params: wmsUrlParams.value,
+        tileGrid: !positionStore.projection.usesMercatorPyramid
             ? new TileGrid({
-                resolutions: projection.value.getResolutionSteps().map((step) => step.resolution),
-                extent: projection.value.bounds.flatten,
-                origin: projection.value.getTileOrigin(),
-                tileSize: WMS_TILE_SIZE,
-            })
-            : undefined
+                  resolutions: positionStore.projection
+                      .getResolutionSteps()
+                      .map((step) => step.resolution),
+                  extent: positionStore.projection.bounds?.flatten,
+                  origin: positionStore.projection.getTileOrigin(),
+                  tileSize: WMS_TILE_SIZE,
+              })
+            : undefined,
+    })
+}
 
-        source = new TileWMS({
-            projection: projection.value.epsg,
-            url: url.value,
-            gutter: gutter.value,
-            params: wmsUrlParams.value,
-            tileGrid: tileGrid,
-        })
-    } else {
-        source = new ImageWMS({
-            url: url.value,
-            projection: projection.value.epsg,
-            params: wmsUrlParams.value,
-            // Limiting image request to exactly the size of the map viewport.
-            // We have a couple layers that state when they have lastly been updated at the bottom
-            // of the WMS image, and without this ratio prop this label is out of the map viewport.
-            // (e.g. ch.bazl.luftfahrthindernis)
-            ratio: 1,
-        })
-    }
-    return source
+function createImageWMSSource(): ImageWMS {
+    return new ImageWMS({
+        url: url.value,
+        projection: positionStore.projection.epsg,
+        params: wmsUrlParams.value,
+        // Limiting image request to exactly the size of the map viewport.
+        // We have a couple layers that state when they have lastly been updated at the bottom
+        // of the WMS image, and without this ratio prop this label is out of the map viewport.
+        // (e.g. ch.bazl.luftfahrthindernis)
+        ratio: 1,
+    })
 }
 
 // If the layer config comes with an extent, we set it up to both types of WMS layer.
 // That means that data will not be requested if the map viewport is outside the extent.
-function setExtent(): void {
-    if (props.wmsLayerConfig.extent) {
-        layer.setExtent(extentUtils.flattenExtent(props.wmsLayerConfig.extent))
-    } else if (props.wmsLayerConfig.constructor.name === 'GeoAdminWMSLayer') {
+function setExtent() {
+    if (wmsLayerConfig.extent) {
+        layer.setExtent(extentUtils.flattenExtent(wmsLayerConfig.extent))
+    } else if (!wmsLayerConfig.isExternal) {
         // do not request stuff outside our technical extent with our own layers.
-        const bounds = LV95.getBoundsAs(projection.value)
-        if (bounds) {
-            layer.setExtent(bounds.flatten)
-        }
+        layer.setExtent(LV95.getBoundsAs(positionStore.projection)?.flatten)
     }
 }
 </script>
