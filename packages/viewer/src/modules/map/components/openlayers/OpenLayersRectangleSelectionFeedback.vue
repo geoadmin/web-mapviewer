1e4127ba1d5d6dbc0d1359be8ca6f55a50709d25
Ismail Sunni <imajimatika@gmail.com>
2025-10-13 18:34:09 +0700

    PB-1383: Migrate OL maps component part 2.

--- packages/viewer/src/modules/map/components/openlayers/OpenLayersRectangleSelectionFeedback.vue
+++ packages/viewer/src/modules/map/components/openlayers/OpenLayersRectangleSelectionFeedback.vue
@@ -1,34 +1,39 @@
-<script setup lang="js">
+<script setup lang="ts">
+import type { Map } from 'ol'
+
 import Feature from 'ol/Feature'
 import { Polygon } from 'ol/geom'
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
 import { v4 as uuidv4 } from 'uuid'
 import { computed, inject, watch } from 'vue'
-import { useStore } from 'vuex'
 
+import useMapStore from '@/store/modules/map.store'
 import { selectionBoxStyle } from '@/utils/styleUtils'
 
-const store = useStore()
-const lastClick = computed(() => store.state.map.clickInfo)
-const selectionPolygon = computed(() => {
-    if (lastClick.value?.coordinate.length === 4) {
-        const bottomLeft = [lastClick.value.coordinate[0], lastClick.value.coordinate[1]]
-        const bottomRight = [lastClick.value.coordinate[2], lastClick.value.coordinate[1]]
-        const topRight = [lastClick.value.coordinate[2], lastClick.value.coordinate[3]]
-        const topLeft = [lastClick.value.coordinate[0], lastClick.value.coordinate[3]]
+const mapStore = useMapStore()
+const rectangleExtent = computed(() => mapStore.rectangleSelectionExtent)
+const selectionPolygon = computed((): Feature<Polygon> | undefined => {
+    if (rectangleExtent.value && rectangleExtent.value.length === 4) {
+        const [minX, minY, maxX, maxY] = rectangleExtent.value
+        const bottomLeft = [minX, minY]
+        const bottomRight = [maxX, minY]
+        const topRight = [maxX, maxY]
+        const topLeft = [minX, maxY]
         return new Feature({
             geometry: new Polygon([[bottomLeft, bottomRight, topRight, topLeft, bottomLeft]]),
         })
     }
-    return null
+    return undefined
 })
 
 // Create a VectorLayer to show the selection feedback
-const map = inject('olMap')
+const map = inject<Map>('olMap')!
 const vectorLayer = new VectorLayer({
-    id: 'rectangleSelectionFeedback',
-    uuid: uuidv4(),
+    properties: {
+        id: 'rectangleSelectionFeedback',
+        uuid: uuidv4(),
+    },
     source: new VectorSource({
         features: [],
     }),
@@ -39,11 +44,14 @@ const vectorLayer = new VectorLayer({
 
 watch(selectionPolygon, () => updateLayer())
 
-function updateLayer() {
+function updateLayer(): void {
     map.removeLayer(vectorLayer)
-    vectorLayer.getSource().clear()
-    if (selectionPolygon.value) {
-        vectorLayer.getSource().addFeature(selectionPolygon.value)
+    const source = vectorLayer.getSource() as VectorSource<Feature<Polygon>>
+    if (source) {
+        source.clear()
+    }
+    if (selectionPolygon.value && source) {
+        source.addFeature(selectionPolygon.value)
         map.addLayer(vectorLayer)
     }
 }
