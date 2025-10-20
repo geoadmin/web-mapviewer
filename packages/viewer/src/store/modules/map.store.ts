922ed7226ab6d725addce35f481cb3c16a95c499
Pascal Barth <pak.barth@gmail.com>
2025-10-20 11:34:25 +0200

    PB-1383: merge click plugin into map store

--- packages/viewer/src/store/modules/map.store.ts
+++ packages/viewer/src/store/modules/map.store.ts
@@ -1,11 +1,16 @@
 import type { FlatExtent, SingleCoordinate } from '@swissgeo/coordinates'
 
+import log, { LogPreDefinedColor } from '@swissgeo/log'
 import { defineStore } from 'pinia'
 
 import type { SelectableFeature } from '@/api/features.api'
 import type { ActionDispatcher } from '@/store/types'
 
 import { MapStoreActions } from '@/store/actions'
+import useDrawingStore from '@/store/modules/drawing.store'
+import useFeaturesStore, { IdentifyMode } from '@/store/modules/features.store'
+import useLayersStore from '@/store/modules/layers.store'
+import useUIStore, { FeatureInfoPositions } from '@/store/modules/ui.store'
 
 export enum ClickType {
     /* Any action that triggers the context menu, so for example, right click with a mouse or
@@ -63,6 +68,72 @@ const useMapStore = defineStore('map', {
         /** Sets all information about the last click that occurred on the map* */
         [MapStoreActions.Click](clickInfo: ClickInfo | undefined, dispatcher: ActionDispatcher) {
             this.clickInfo = clickInfo
+
+            const drawingStore = useDrawingStore()
+            const featuresStore = useFeaturesStore()
+            const layersStore = useLayersStore()
+            const uiStore = useUIStore()
+
+            if (clickInfo && !drawingStore.drawingOverlay.show) {
+                // if a click occurs, we only take it into account (for identify and fullscreen toggle)
+                // when the user is not currently drawing something on the map.
+                const isCtrlLeftSingleClick = clickInfo.clickType === ClickType.CtrlLeftSingleClick
+                const isContextMenuClick = clickInfo.clickType === ClickType.ContextMenu
+                const isIdentifyingFeature =
+                    clickInfo.clickType === ClickType.LeftSingleClick ||
+                    clickInfo.clickType === ClickType.CtrlLeftSingleClick ||
+                    clickInfo.clickType === ClickType.DrawBox
+
+                if (isIdentifyingFeature) {
+                    const identifyMode = isCtrlLeftSingleClick
+                        ? IdentifyMode.Toggle
+                        : IdentifyMode.New
+
+                    if (clickInfo.features) {
+                        featuresStore
+                            .identifyFeatureAt(
+                                {
+                                    layers: layersStore.visibleLayers.filter(
+                                        (layer) => layer.hasTooltip
+                                    ),
+                                    vectorFeatures: clickInfo.features,
+                                    coordinate: clickInfo.coordinate,
+                                    identifyMode: identifyMode,
+                                },
+                                dispatcher
+                            )
+                            .then(() => {
+                                if (
+                                    uiStore.noFeatureInfo &&
+                                    featuresStore.selectedFeaturesByLayerId.length > 0
+                                ) {
+                                    // we only change the feature Info position when it's set to 'NONE', as
+                                    // we want to keep the user's choice of position between clicks.
+                                    uiStore.setFeatureInfoPosition(
+                                        FeatureInfoPositions.DEFAULT,
+                                        dispatcher
+                                    )
+                                }
+                            })
+                            .catch((error) =>
+                                log.error({
+                                    title: 'Map Store / Click',
+                                    titleColor: LogPreDefinedColor.Cyan,
+                                    messages: [
+                                        'Error while identifying feature after a click on the map',
+                                        clickInfo,
+                                        dispatcher,
+                                        error,
+                                    ],
+                                })
+                            )
+                    }
+                }
+
+                if (isContextMenuClick) {
+                    this.setLocationPopupCoordinates(clickInfo.coordinate, dispatcher)
+                }
+            }
         },
 
         [MapStoreActions.ClearClick](dispatcher: ActionDispatcher) {
