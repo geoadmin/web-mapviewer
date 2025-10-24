7573cbc7bc22d3008cbb3af9f6d1679e107c2898
Pascal Barth <pak.barth@gmail.com>
2025-10-24 15:22:30 +0200

    PB-1979: clean up

--- packages/viewer/src/store/modules/ui/actions/setSize.ts
+++ packages/viewer/src/store/modules/ui/actions/setSize.ts
@@ -1,8 +1,11 @@
 import type { UIStore } from '@/store/modules/ui/types/ui'
 import type { ActionDispatcher } from '@/store/types'
 
-import { MAX_WIDTH_SHOW_FLOATING_TOOLTIP } from '@/config/responsive.config'
-import { BREAKPOINT_PHONE_HEIGHT, BREAKPOINT_PHONE_WIDTH } from '@/config/responsive.config'
+import {
+    BREAKPOINT_PHONE_HEIGHT,
+    BREAKPOINT_PHONE_WIDTH,
+    MAX_WIDTH_SHOW_FLOATING_TOOLTIP,
+} from '@/config/responsive.config'
 import { UIModes } from '@/store/modules/ui'
 import { FeatureInfoPositions } from '@/store/modules/ui/types/featureInfoPositions.enum'
 
@@ -22,16 +25,8 @@ export default function setSize(
         this.featureInfoPosition = FeatureInfoPositions.BottomPanel
     }
 
-    updateUiModeFromScreenSize.call(this, dispatcher)
-}
-
-function updateUiModeFromScreenSize(this: UIStore, dispatcher: ActionDispatcher): void {
-    let wantedUiMode
-
-    if (
-        this.width < BREAKPOINT_PHONE_WIDTH ||
-        this.height < BREAKPOINT_PHONE_HEIGHT
-    ) {
+    let wantedUiMode: UIModes
+    if (this.width < BREAKPOINT_PHONE_WIDTH || this.height < BREAKPOINT_PHONE_HEIGHT) {
         wantedUiMode = UIModes.Phone
     } else {
         // so the UI mode Desktop also includes the tablet mode.
