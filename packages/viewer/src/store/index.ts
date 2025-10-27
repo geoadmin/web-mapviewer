796308e0b58fee40960c7b05c0a1664a7d31919e
Pascal Barth <pak.barth@gmail.com>
2025-10-27 10:50:00 +0100

    PB-1979: remove unused topic plugin
    It look like it was already merged with the store when I fixed the app loading logic. I tried switching a couple topics, switching lang etc... and everything seems to be working fine without changing the topic store code.

--- packages/viewer/src/store/index.ts
+++ packages/viewer/src/store/index.ts
@@ -2,12 +2,10 @@ import { createPinia } from 'pinia'
 
 import reprojectPlugin from '@/store/plugins/reproject.plugin'
 import storeActionLogPlugin from '@/store/plugins/store-action-log.plugin'
-import topicChangeManagement from '@/store/plugins/topic-change-management.plugin'
 import updateSelectedFeatures from '@/store/plugins/update-selected-features.plugin'
 
 const pinia = createPinia()
 pinia.use(reprojectPlugin)
-pinia.use(topicChangeManagement)
 pinia.use(updateSelectedFeatures)
 pinia.use(storeActionLogPlugin)
 
