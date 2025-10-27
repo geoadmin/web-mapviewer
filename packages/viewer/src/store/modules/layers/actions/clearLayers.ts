66ec6ecc1148ffb5015b50c59ef6919ed50a9f87
Felix Sommer <felix.sommer@camptocamp.com>
2025-10-27 13:19:12 +0100

    PB-1979: refactor update selected features plugin

--- packages/viewer/src/store/modules/layers/actions/clearLayers.ts
+++ packages/viewer/src/store/modules/layers/actions/clearLayers.ts
@@ -1,6 +1,9 @@
 import type { LayersStore } from '@/store/modules/layers/types/layers'
 import type { ActionDispatcher } from '@/store/types'
 
+import { identifyFeatures } from '@/store/modules/layers/utils/identifyFeatures'
+
 export default function clearLayers(this: LayersStore, dispatcher: ActionDispatcher) {
     this.activeLayers = []
+    identifyFeatures.call(this, undefined, { this: this }, dispatcher)
 }
