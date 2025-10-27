7f37ac20429d6a7a6d6b756da83970cda493f260
Pascal Barth <pak.barth@gmail.com>
2025-10-27 12:05:52 +0100

    PB-1979: migrate profile/stitchMultiLine utils function unit test to TS

--- packages/viewer/src/store/modules/profile/utils/__tests__/stitchMultiLine.spec.ts
+++ packages/viewer/src/store/modules/profile/utils/__tests__/stitchMultiLine.spec.ts
@@ -1,8 +1,10 @@
+import type { SingleCoordinate } from '@swissgeo/coordinates'
+
 import { describe, expect, it } from 'vitest'
 
-import { stitchMultiLine } from '../profile'
+import { stitchMultiLine } from '../stitchMultiLine'
 
-function checkLines(expectedResult, result) {
+function checkLines(expectedResult: SingleCoordinate[][], result: SingleCoordinate[][]) {
     expect(result).to.be.an('Array')
     expect(result).to.have.lengthOf(
         expectedResult.length,
@@ -17,7 +19,7 @@ function checkLines(expectedResult, result) {
                 `Result line at index ${index} length mismatch\n\nexpected: ${JSON.stringify(line)}\ngot: ${JSON.stringify(resultLine)}\n\n`
             )
         line.forEach((point, index) => {
-            expect(resultLine[index]).to.deep.equal(
+            expect(resultLine![index]).to.deep.equal(
                 point,
                 `Line ${index} point ${index}\n\nexpected: ${JSON.stringify(expectedResult)}\ngot: ${JSON.stringify(result)}\n\n`
             )
@@ -28,7 +30,7 @@ function checkLines(expectedResult, result) {
 describe('Test function stitchMultiLine', () => {
     const testTolerance = 10.0
     it('Returns the input if there is nothign to stitch', () => {
-        const input = [
+        const input: SingleCoordinate[][] = [
             [
                 [1, 1],
                 [2, 2],
@@ -42,7 +44,7 @@ describe('Test function stitchMultiLine', () => {
         checkLines(input, output)
     })
     it('Stitches lines if they are less than or exactly 10m apart', () => {
-        const input = [
+        const input: SingleCoordinate[][] = [
             [
                 [1, 1],
                 [2, 2],
@@ -66,7 +68,7 @@ describe('Test function stitchMultiLine', () => {
         )
     })
     it('Stitches multiples segment into one if it is possible', () => {
-        const input = [
+        const input: SingleCoordinate[][] = [
             [
                 [1, 1],
                 [2, 2],
@@ -112,7 +114,7 @@ describe('Test function stitchMultiLine', () => {
         )
     })
     it('stitches multiples segment into one when some are inverted', () => {
-        const input = [
+        const input: SingleCoordinate[][] = [
             [
                 [1, 1],
                 [2, 2],
