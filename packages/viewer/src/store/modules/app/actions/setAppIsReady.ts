2fa591b5300e0bd7838ae1b5489a8c7d4b5e8895
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2025-11-10 17:39:35 +0100

    PB-2073: adding the map module readiness to the states

--- packages/viewer/src/store/modules/app/actions/setAppIsReady.ts
+++
@@ -1,7 +0,0 @@
-import type { ActionDispatcher } from '@/store/types'
-
-import { AppStates, type AppStore } from '@/store/modules/app/types/app'
-
-export default function setAppIsReady (this: AppStore, dispatcher: ActionDispatcher) {
-    this.appState = AppStates.ConfigLoaded
-}
