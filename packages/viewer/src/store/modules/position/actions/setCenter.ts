e49f825275c9adc9ad525c64d845fd008b2cd907
Ismail Sunni <imajimatika@gmail.com>
2025-11-12 12:39:23 +0700

    PB-2068: Remove comments.

--- packages/viewer/src/store/modules/position/actions/setCenter.ts
+++ packages/viewer/src/store/modules/position/actions/setCenter.ts
@@ -49,11 +49,8 @@ export default function setCenter(
         dispatcher.name === 'RecenterButton.vue' ||
         dispatcher.name === 'OpenLayersGeolocationFeedback.vue'
 
-    console.log('[setCenter] dispatcher.name:', dispatcher.name, 'tracking:', geolocationStore.tracking, 'position !== center:', geolocationStore.position !== center, 'isFromGeolocation:', isFromGeolocation)
-
     if (geolocationStore.tracking && geolocationStore.position !== center && !isFromGeolocation) {
         // if we moved the map we disabled the geolocation tracking (unless the tracking moved the map)
-        console.log('[setCenter] Disabling tracking because center changed from non-geolocation source')
         geolocationStore.setGeolocationTracking(false, dispatcher)
         this.setAutoRotation(false, dispatcher)
     }
