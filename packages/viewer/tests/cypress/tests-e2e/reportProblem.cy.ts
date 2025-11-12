73c9d2a161d32a5fcb18ee08b32b4f01aa00bb80
Felix Sommer <felix.sommer@camptocamp.com>
2025-11-12 16:53:05 +0100

    PB-2064: report problem

--- packages/viewer/tests/cypress/tests-e2e/reportProblem.cy.ts
+++ packages/viewer/tests/cypress/tests-e2e/reportProblem.cy.ts
@@ -309,7 +309,7 @@ describe('Testing the report problem form', () => {
             cy.get('[data-cy="ol-map"]').click(mapWidth / 2 + 50, mapHeight / 2)
         })
         // we need to increase the timeout here below because, upon opening the drawing mode for the
-        // first time in e2e tests, the loading of the library can take time
+        // first time in e2e tests, the loading of the library can take more time 
         cy.get('[data-cy="drawing-header-title"]', { timeout: 15000 })
             .should('be.visible')
             .contains('3. Indicate the appropriate location on the map :')
