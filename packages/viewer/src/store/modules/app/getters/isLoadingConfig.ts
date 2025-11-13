6235b58c45e022d80a8c88b801591a5b837798bc
Pascal Barth <pak.barth@gmail.com>
2025-11-13 09:12:34 +0100

    PB-2073: clean-up

--- packages/viewer/src/store/modules/app/getters/isLoadingConfig.ts
+++ packages/viewer/src/store/modules/app/getters/isLoadingConfig.ts
@@ -2,8 +2,6 @@ import type { AppStore } from '@/store/modules/app/types/app'
 
 import { AppStateNames } from '@/store/modules/app/types/appState'
 
-// This is a flag to tell if the layer config / topics  are loaded.
-// We should rename it once there is less people working on every file
 export default function isLoadingConfig(this: AppStore): boolean {
     return this.appState.name === AppStateNames.Initializing
 }
