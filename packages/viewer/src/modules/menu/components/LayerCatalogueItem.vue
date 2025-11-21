fa58754c8a33cfbf9a30c55e85f536cecccac343
Felix Sommer <felix.sommer@camptocamp.com>
2025-11-21 15:02:46 +0100

    PB-2064: fix import map layers

--- packages/viewer/src/modules/menu/components/LayerCatalogueItem.vue
+++ packages/viewer/src/modules/menu/components/LayerCatalogueItem.vue
@@ -6,7 +6,7 @@
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { type FlatExtent, LV95, type NormalizedExtent } from '@swissgeo/coordinates'
-import { type GeoAdminGroupOfLayers, type Layer, LayerType } from '@swissgeo/layers'
+import { type ExternalWMSLayer, type GeoAdminGroupOfLayers, type Layer, LayerType } from '@swissgeo/layers'
 import log from '@swissgeo/log'
 import { booleanContains, polygon } from '@turf/turf'
 import { computed, onMounted, ref, watch } from 'vue'
@@ -61,8 +61,11 @@ const showItem = computed(() => {
 const isGroupOfLayers = (layer: Layer): layer is GeoAdminGroupOfLayers => {
     return layer.type === LayerType.GROUP
 }
+const isWmsLayer = (layer: Layer): layer is ExternalWMSLayer => {
+    return layer.type === LayerType.WMS
+}
 
-const hasChildren = computed(() => isGroupOfLayers(item) && item?.layers?.length > 0)
+const hasChildren = computed(() => (isGroupOfLayers(item) ||isWmsLayer(item)) && item?.layers?.length && item?.layers?.length > 0)
 const hasDescription = computed(() => canBeAddedToTheMap.value && item?.hasDescription)
 const isPhoneMode = computed(() => uiStore.isPhoneMode)
 
@@ -320,7 +323,7 @@ function containsLayer(layers: Layer[], searchText: string): boolean {
             </button>
         </div>
         <ul
-            v-if="showChildren && isGroupOfLayers(item)"
+            v-if="showChildren && (isGroupOfLayers(item) || isWmsLayer(item))"
             class="menu-catalogue-item-children"
             :class="`ps-${2 + depth}`"
         >
