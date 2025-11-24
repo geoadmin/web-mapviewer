150b0e5e435f8e085d3dc892dd2296214568e4b5
Felix Sommer <felix.sommer@camptocamp.com>
2025-11-24 13:52:16 +0100

    PB-2064: fix hasDataDisclaimer whitelisting

--- packages/viewer/src/store/modules/layers/getters/hasDataDisclaimer.ts
+++ packages/viewer/src/store/modules/layers/getters/hasDataDisclaimer.ts
@@ -4,13 +4,24 @@ import { LayerType } from '@swissgeo/layers'
 
 import type { LayerActionFilter, LayersStore } from '@/store/modules/layers/types/layers'
 
+import { EXTERNAL_PROVIDER_WHITELISTED_URL_REGEXES } from '@/config/regex.config'
+
 export default function hasDataDisclaimer(
     this: LayersStore
 ): (layerId: string, options?: LayerActionFilter) => boolean {
     return (layerId: string, options?: LayerActionFilter) =>
         this.getActiveLayersById(layerId, options).some(
             (layer: Layer) =>
-                layer &&
-                (layer.isExternal || (layer.type === LayerType.KML && !(layer as KMLLayer).adminId))
+                (layer && layer.isExternal && !checkLayerUrlWhitelisting(options?.baseUrl)) ||
+                (layer.type === LayerType.KML && !(layer as KMLLayer).adminId)
         )
+}
+
+function checkLayerUrlWhitelisting(layerBaseUrl?: string): boolean {
+    if (!layerBaseUrl) {
+        return false
+    }
+    return !!EXTERNAL_PROVIDER_WHITELISTED_URL_REGEXES.find(
+        (regex) => !!layerBaseUrl.match(regex)
+    )
 }
