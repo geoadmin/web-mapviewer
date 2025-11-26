c4ac4bd94f4ce0932e92ea4b93d5664775b65f74
Felix Sommer <felix.sommer@camptocamp.com>
2025-11-26 09:01:40 +0100

    PB-2064: fix wmts layer parsing import map

--- packages/layers/src/index.ts
+++ packages/layers/src/index.ts
@@ -1,6 +1,13 @@
+import { registerProj4 } from '@swissgeo/coordinates'
+import proj4 from 'proj4'
+
 // let's export the types "globally"
 export * from '@/types/layers'
 export * from '@/types/capabilities'
 export * from '@/types/timeConfig'
 
 export * from '@/validation'
+import { register } from 'ol/proj/proj4'
+
+registerProj4(proj4)
+register(proj4)
