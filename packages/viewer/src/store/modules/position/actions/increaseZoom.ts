66719068e3e7bb14c633aa45a98ccb38bf1ad73d
Ismail Sunni <imajimatika@gmail.com>
2025-12-12 06:19:44 +0700

    PB-2064: Fix missing return when changing zoom.

--- packages/viewer/src/store/modules/position/actions/increaseZoom.ts
+++ packages/viewer/src/store/modules/position/actions/increaseZoom.ts
@@ -8,6 +8,7 @@ export default function increaseZoom(this: PositionStore, dispatcher: ActionDisp
         // for Swiss coordinate system, there's an extra param to trigger normalization
         // (snapping to the closest rounded value)
         this.zoom = this.projection.roundZoomLevel(this.zoom, true) + 1
+        return
     }
     this.zoom = this.projection.roundZoomLevel(this.zoom) + 1
 }
