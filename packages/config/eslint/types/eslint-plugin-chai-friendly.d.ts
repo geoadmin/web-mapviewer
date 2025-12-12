1c19b62c3c6015ccbc255d26e5f9668acbde697d
Pascal Barth <pak.barth@gmail.com>
2025-12-12 09:37:07 +0100

    PB-1383: export ESLint config as JS

---
+++ packages/config/eslint/types/eslint-plugin-chai-friendly.d.ts
@@ -0,0 +1,7 @@
+declare module 'eslint-plugin-chai-friendly' {
+    import { FlatConfig } from '@typescript-eslint/utils/ts-eslint'
+
+    const plugin: FlatConfig.Plugin
+
+    export default plugin
+}
