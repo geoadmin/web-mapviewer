3b454c5135ab4b73b1b76e6f8d9970d7d1ca48a7
Ismail Sunni <imajimatika@gmail.com>
2025-12-31 12:57:31 +0700

    PB-2064: Remove console.log

--- packages/viewer/src/utils/searchParamUtils.ts
+++ packages/viewer/src/utils/searchParamUtils.ts
@@ -7,9 +7,6 @@
  */
 export function removeQueryParamFromHref(key: string): void {
     const [baseUrl, queryString] = window.location.href.split('?')
-    console.log('[removeQueryParamFromHref] baseUrl', baseUrl)
-    console.log('[removeQueryParamFromHref] queryString', queryString)
-
     if (!queryString) {
         return
     }
@@ -19,11 +16,8 @@ export function removeQueryParamFromHref(key: string): void {
         return
     }
     params.delete(key)
-    console.log('[removeQueryParamFromHref] replaced key', key)
 
     const newQueryString = params.toString()
-    console.log('[removeQueryParamFromHref] newQueryString', newQueryString)
     const newUrl = newQueryString ? `${baseUrl}?${newQueryString}` : baseUrl
-    console.log('[removeQueryParamFromHref] newUrl', newUrl)
     window.history.replaceState({}, document.title, newUrl)
 }
