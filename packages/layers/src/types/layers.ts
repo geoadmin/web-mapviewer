3ed0e748ad92507e309ee3507b8073336cd0f5a2
Felix Sommer <felix.sommer@camptocamp.com>
2026-01-20 11:20:13 +0100

    PB-2064: refactor geojson layer geojson data

--- packages/layers/src/types/layers.ts
+++ packages/layers/src/types/layers.ts
@@ -206,7 +206,7 @@ export interface GeoAdminGeoJSONLayer extends GeoAdminLayer {
     /** The URL to use when requesting the GeoJSON data (the true GeoJSON per se...) */
     readonly geoJsonUrl: string
     geoJsonStyle?: GeoAdminGeoJSONStyleDefinition
-    geoJsonData?: GeoJsonData | string
+    geoJsonData?: GeoJsonData
 }
 
 export interface GeoJsonData {
