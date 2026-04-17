2b1b9c3c93f5db09e4b4213672e0cc98dee3e60b
Martin Künzi <Martin.Kuenzi@swisstopo.ch>
2026-04-17 20:28:30 +0200

    PB-2553: stopping XSS attacks by whitelisting
    - Issue:
     The api_url, wms_url and wmts_url params can currently receive any url as values, which means a malicious party could use them as an attack vector.
     This is especially true for api_url, which can send html back and is needed to operate that way.
    - Fix:
     We have made regexes which enfore a whitelist on those parameters
     As we need external sources html to be rendered (for the feature informations, for example), we sadly can't sanitize the html given to us without breaking a lot of currently working layers. We can only limit the sources and hope they don't provide malicious content.

--- packages/mapviewer/src/router/storeSync/BaseUrlOverrideParamConfig.class.js
+++ packages/mapviewer/src/router/storeSync/BaseUrlOverrideParamConfig.class.js
@@ -6,9 +6,22 @@ import {
 import AbstractParamConfig from '@/router/storeSync/abstractParamConfig.class'
 import { isValidUrl } from '@/utils/utils'
 
+// //https://sys-{wms,wmts,api3}.{dev,int}.bgdi.ch/
+// or http://localhost{:port_number}
+const allowed_url_regexes = {
+    wms_url: /^https:\/\/sys-wms.(dev|int).bgdi.ch\/?$/g,
+    wmts_url: /^https:\/\/sys-wmts.(dev|int).bgdi.ch\/?$/g,
+    api3: /^https:\/\/sys-api3.(dev|int).bgdi.ch\/?$/g,
+    localhost: /^http:\/\/localhost(:\d{2,6})?\/?$/g,
+}
+
 export default function createBaseUrlOverrideParamConfig({ urlParamName, baseUrlPropertyName }) {
     function dispatchBaseUrlOverride(to, store, urlParamValue) {
-        if (isValidUrl(urlParamValue)) {
+        if (
+            isValidUrl(urlParamValue) &&
+            (allowed_url_regexes[urlParamName]?.match(urlParamValue) ||
+                allowed_url_regexes.localhost.match(urlParamValue))
+        ) {
             setBaseUrlOverrides(baseUrlPropertyName, urlParamValue)
         } else {
             setBaseUrlOverrides(baseUrlPropertyName, null)
