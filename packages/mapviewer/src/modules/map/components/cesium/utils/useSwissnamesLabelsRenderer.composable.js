085d04d835b540f44033de970c2be623d47aabf7
Raphael Antonietti <raphael.antonietti@swisstopo.ch>
2026-07-28 14:41:08 +0200

    PB-2246: use filtered Swissnames publication

--- packages/mapviewer/src/modules/map/components/cesium/utils/useSwissnamesLabelsRenderer.composable.js
+++ packages/mapviewer/src/modules/map/components/cesium/utils/useSwissnamesLabelsRenderer.composable.js
@@ -15,7 +15,7 @@ import {
 } from 'cesium'
 import { onBeforeUnmount, onMounted } from 'vue'
 
-const PUBLICATION_PATH = 'v3/tileset.json'
+const PUBLICATION_PATH = 'v4-language-experiment/tileset.json'
 const HEIGHT_OFFSET = 2
 const FADE_START_RATIO = 0.76
 const BACKGROUND_COLOR = Color.fromCssColorString('#15191e').withAlpha(0.94)
